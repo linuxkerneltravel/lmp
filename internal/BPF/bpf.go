55db5b66404063d4b887f4135a7c4f4730d32e2f
zcy <lonemorelightp@163.com>
2020-04-16 00:25:16 +0800

    Build BPF generator using the builder pattern

--- internal/BPF/bpf.go
+++ internal/BPF/bpf.go
@@ -1,14 +1,39 @@
-package BPF
+package bpf
 
-import (
-	"lmp_ui/deployments/message"
-	"os"
-)
+type DirectorPy struct {
+	//生成python文件的接口
+	pyBuilder PyBuilder
+}
 
-type BPFBody struct{}
+type DirectorC struct {
+	//生成C文件的接口
+	cBuilder CBuilder
+}
 
-func (b *BPFBody)Generator(m *message.ConfigMessage) os.File {
-	return os.File{}
+func NewDirectorPy(b PyBuilder) DirectorPy {
+	return DirectorPy{
+		pyBuilder : b,
+	}
+}
+
+func NewDirectorC(c CBuilder) DirectorC {
+	return DirectorC{
+		cBuilder : c,
+	}
+}
+
+//生成python文件的步骤，接口和实现分离
+func (d *DirectorPy)ConstructPy() {
+	d.pyBuilder.AddCommonPyFileFront()
+	d.pyBuilder.AddPrivatePyFile()
+	d.pyBuilder.AddCommonPyFileEnd()
+}
+
+//生成C文件的步骤，接口和实现分离
+func (d *DirectorC)ConstructC() {
+	d.cBuilder.AddCommonCFileFront()
+	d.cBuilder.AddPrivateCFile()
+	d.cBuilder.AddCommonCFileEnd()
 }
 
 
