99fa12302cf5d8b73675ae165b2ab42aa3b0e359
zcy <lonemorelightp@163.com>
2020-04-21 01:46:40 +0800

    Minimum available systems implemented

--- api/handle.go
+++ api/handle.go
@@ -1,11 +1,13 @@
 package api
 
 import (
-	"fmt"
-	"lmp/deployments/sys"
 	"lmp/pkg/model"
-
+	"lmp/config"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"path"
+	"os/exec"
+	"github.com/cihub/seelog"
 )
 
 func init() {
@@ -17,19 +19,72 @@ func init() {
 	})
 }
 
-func do_collect(c *Context) {
-	data := sys.Data{}
-	m := fillConfigMessage(c)
+func do_collect(c *Context)  {
+	//data := sys.Data{}
+	//m := fillConfigMessage(c)
 	//TODO..
 
-	fmt.Println(m)
-	data.Handle(&m)
-	c.JSON(200, gin.H{"message": "do_collect"})
+	//fmt.Println(m)
+	//data.Handle(&m)
+
+	// For static bpf files
+	go execute()
+
+	c.Redirect(http.StatusMovedPermanently,"http://127.0.0.1:3000/")
+
+	return
 }
+
 
 func Ping(c *Context) {
 	c.JSON(200, gin.H{"message": "pong"})
 }
+
+
+func execute() {
+	collector := path.Join(config.DefaultCollectorPath, "collect.py")
+	cmd := exec.Command("python", collector)
+	stdout, err := cmd.StdoutPipe();
+	if err != nil {
+		seelog.Error(err)
+		return
+	}
+	defer stdout.Close();
+
+	stderr, err := cmd.StderrPipe();
+	if  err != nil {
+		seelog.Error(err)
+		return
+	}
+	defer stderr.Close()
+	//go func() {
+	//	scanner := bufio.NewScanner(stdout)
+	//	for scanner.Scan() {
+	//		line := scanner.Text()
+	//		fmt.Println(line)
+	//	}
+	//}()
+	//
+	//go func() {
+	//	scanner := bufio.NewScanner(stderr)
+	//	for scanner.Scan() {
+	//		line := scanner.Text()
+	//		fmt.Println(line)
+	//	}
+	//}()
+	err = cmd.Start()
+	if  err != nil {
+		seelog.Error(err)
+		return
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		seelog.Error(err)
+		return
+	}
+}
+
 
 func fillConfigMessage(c *Context) model.ConfigMessage {
 	var m model.ConfigMessage
