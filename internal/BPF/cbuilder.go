0b18a3d671adcb05d1c873407743d0ed15546384
ssssx <Chris_69@163.com>
2020-05-05 09:00:17 -0700

    revise cbuilder code

--- internal/BPF/cbuilder.go
+++ internal/BPF/cbuilder.go
@@ -1,13 +1,10 @@
 package bpf
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
 	"lmp/config"
+	"lmp/deployments/common"
 	"lmp/internal/bpfcode"
 	"os"
-	"strings"
 )
 
 //定义生成者模式中的Builder，这里的CBuilder是C文件生成的方法
@@ -18,97 +15,42 @@ type CBuilder interface {
 }
 
 //具体来创建python文件的ConcreteBuilder
-/*
-type ConcreteBuilderC struct {
-	f os.File
-	status bool
-}
-*/
 
 type ConcreteBuilderC struct {
 	f *os.File
-	//	status bool
 }
 
 //返回ConcreteBuilderC的实例
 func NewConcreteBuilderC() ConcreteBuilderC {
 	return ConcreteBuilderC{
-		f: CreatCfile(config.BpfPath),
-		//		status: false,
+		f: common.Creatfile(config.BpfPath),
 	}
 }
 
 //实现接口的方法
 func (c *ConcreteBuilderC) AddCommonCFileFront() {
-	//把bpfcode下面的bpf.c中的内容复制给path文件
-	f1, err1 := os.Open("./internal/bpfcode/bpf.txt")
-	if err1 != nil {
-		fmt.Println("bpf.c open failed")
-	}
-	f2, err2 := os.OpenFile(config.BpfPath, os.O_WRONLY|os.O_CREATE, os.ModePerm)
-	if err2 != nil {
-		fmt.Println("copy to initCfile failed")
-	}
-	defer f1.Close()
-	defer f2.Close()
-	io.Copy(f2, f1)
+
+	common.CopyFileContext("./internal/bpfcode/bpf.txt", config.BpfPath)
 
 }
 func (c *ConcreteBuilderC) AddPrivateCFile() {
-	//替换bpf.c中的VFSSTAT-DATATYPE字符串
-	oldContext, err := ioutil.ReadFile(config.BpfPath)
-	if err != nil {
-		fmt.Println("read initCfile failed")
-	}
-	newContext := strings.Replace(string(oldContext), "VFSSTAT-DATATYPE", bpfcode.Vfsstatdatatype, 1)
-	err = ioutil.WriteFile(config.BpfPath, []byte(newContext), 0644)
-	if err != nil {
-		fmt.Println("write firstsstepcontext failed")
-	}
+
+	common.ReplceStringInFile(config.BpfPath, "VFSSTAT-DATATYPE", bpfcode.Vfsstatdatatype)
 
 }
 func (c *ConcreteBuilderC) AddCommonCFileEnd() {
-	oldContext, err := ioutil.ReadFile(config.BpfPath)
-	if err != nil {
-		fmt.Println("read fistStepCfile failed")
-	}
-	newContext := strings.Replace(string(oldContext), "VFSSTAT-CODE", bpfcode.Vfsstatcode, 1)
-	err = ioutil.WriteFile(config.BpfPath, []byte(newContext), 0644)
-	if err != nil {
-		fmt.Println("write finalfile failed")
-	}
 
+	common.ReplceStringInFile(config.BpfPath, "VFSSTAT-CODE", bpfcode.Vfsstatcode)
 }
 
 //最后生成的成果
 type ProductC struct {
 	f *os.File
-	//	status bool
 }
 
 //生成成果，
 func (b *ConcreteBuilderC) GetResultC() ProductC {
 	return ProductC{
-		f: GetCfile(),
-		//		status: true,
+		f: common.Getfile(),
 	}
-}
-
-//生成初始c文件返回文件指针
-func CreatCfile(path string) *os.File {
-	file, err := os.Create(config.BpfPath)
-	if err != nil {
-		fmt.Println("created failed")
-	}
-	defer file.Close()
-	return file
-}
-
-func GetCfile() *os.File {
-	file, err := os.Open(config.BpfPath)
-	if err != nil {
-		fmt.Println("return file failed")
-	}
-	defer file.Close()
-	return file
 }
