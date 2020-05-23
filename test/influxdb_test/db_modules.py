0cc9e344e82f71d86319fcd44c8db360e93d444a
hawl29 <cz_hawl29@163.com>
2020-05-23 08:55:07 +0000

    influxdb python部分，包含写入数据和删除数据两个方法

---
+++ test/influxdb_test/db_modules.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+#-*- coding:utf-8 -*-
+from influxdb import InfluxDBClient
+
+def write2db(datatype,data,client):
+    tmp = [{"measurement":None,"tags":{},"fields":{},}] 
+    tmp[0]["measurement"] = datatype["measurement"]
+    for x in datatype['tags']:
+        tmp[0]["tags"][x] = getattr(data,x)
+    for y in datatype['fields']:
+        tmp[0]["fields"][y] = getattr(data,y)
+    client.write_points(tmp)
+
+def delete_db(client,measurement,*,require):
+    sql = 'delete from ' + measurement + ' where '+ require +';'
+    client.query(sql)
+
