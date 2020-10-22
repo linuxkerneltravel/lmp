11fdc9aca1a7e9156684220a6c8822a3e71136a2
赵晨雨 <lonemorelightp@163.com>
2020-10-22 20:24:19 +0800

    add kuayu

--- controllers/tj.go
+++ controllers/tj.go
@@ -16,3 +16,58 @@ func QueryIRQ(c *gin.Context) {
 
 	ResponseSuccess(c, res)
 }
+
+func QueryCpuUtilize(c *gin.Context) {
+	res, err := logic.DoQueryCpuUtilize()
+	if err != nil {
+		zap.L().Error("ERROR in QueryCpuUtilize():", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
+		return
+	}
+
+	ResponseSuccess(c, res)
+}
+
+func QueryPickNext(c *gin.Context) {
+	res, err := logic.DoQueryPickNext()
+	if err != nil {
+		zap.L().Error("ERROR in QueryPickNext():", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
+		return
+	}
+
+	ResponseSuccess(c, res)
+}
+
+func QueryTaskSwitch(c *gin.Context) {
+	res, err := logic.DoQueryTaskSwitch()
+	if err != nil {
+		zap.L().Error("ERROR in QueryTaskSwitch():", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
+		return
+	}
+
+	ResponseSuccess(c, res)
+}
+
+func QueryHardDiskReadWriteTime(c *gin.Context) {
+	res, err := logic.DoQueryHardDiskReadWriteTime()
+	if err != nil {
+		zap.L().Error("ERROR in QueryHardDiskReadWriteTime():", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
+		return
+	}
+
+	ResponseSuccess(c, res)
+}
+
+func QueryWaterMark(c *gin.Context) {
+	res, err := logic.DoQueryWaterMark()
+	if err != nil {
+		zap.L().Error("ERROR in QueryWaterMark():", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
+		return
+	}
+
+	ResponseSuccess(c, res)
+}
