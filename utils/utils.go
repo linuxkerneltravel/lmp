5d2476845154c2726cd2e6c7f2ac64bb1daaa62d
zcy <lonemorelightp@163.com>
2020-12-04 15:07:37 +0800

    add function

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func PathExist(path string) (bool, error) {
 	_, err := os.Stat(path)
@@ -11,4 +14,16 @@ func PathExist(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func PathIsDir(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err == nil {
+		return info.IsDir(), nil
+	}
+	if os.IsNotExist(err) {
+		return false, fmt.Errorf("path %s doens't exist", path)
+	}
+
+	return false, nil
 }
