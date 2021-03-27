b8969d67d0dbcdab4f918e8df114664271fe3cde
zstone12 <522089185@qq.com>
2021-03-27 16:58:46 +0800

    fix: typo

--- plugins/init_db.py
+++ plugins/init_db.py
@@ -5,7 +5,7 @@ DBNAME = cfg["influxdb"]["dbname"]
 USER = cfg["influxdb"]["user"]
 PASSWORD = cfg["influxdb"]["password"]
 
-influx_client = InfluxDBClient(database=DBNAME,host='129.204.185.247',username=USER,password=PASSWORD)
+influx_client = InfluxDBClient(database=DBNAME,host='localhost',username=USER,password=PASSWORD)
 
 # TODO: 接入其他数据库
 # mysql_client
