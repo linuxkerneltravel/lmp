67ff32af4088967cd192e4a53ba00f2007c7771d
zcy <lonemorelightp@163.com>
2021-04-04 13:02:49 +0800

    alter SQL file

--- misc/init.sql
+++ misc/init.sql
@@ -1,6 +1,6 @@
-drop table if exists performance_info;
-CREATE TABLE if not exists performance_info (
-    id int(11) AUTO_INCREMENT primary key COMMENT '主键',
+drop table if exists performance_index;
+CREATE TABLE if NOT EXISTS performance_index (
+    id int(11) AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
     name varchar(20) NOT NULL unique COMMENT '指标名',
     exec_path varchar(50) NOT NULL COMMENT '指标执行路径',
     instruction varchar(100) COMMENT '指标说明',
