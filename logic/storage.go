6051739fd419ad4467c92c303312be77f66aabf0
zcy <lonemorelightp@163.com>
2021-04-22 15:20:43 +0800

    [Chenyu Zhao] : modification of PluginStorage's methods

--- logic/storage.go
+++ logic/storage.go
@@ -1,13 +1,15 @@
 package logic
 
+import "github.com/linuxkerneltravel/lmp/models"
+
 type PluginStorage struct {
 	pluginStorage map[string]Plugin
 }
 
-func (p *PluginStorage) CreatePluginStorage() PluginStorage {
-	return PluginStorage{}
+func (p *PluginStorage) CreatePluginStorage(message *models.PluginMessage) (*PluginStorage, error) {
+	return &PluginStorage{}, nil
 }
 
-func (p *PluginStorage) CollectData() {
-
+func (p *PluginStorage) CollectData(exitChan chan bool) error {
+	return nil
 }
