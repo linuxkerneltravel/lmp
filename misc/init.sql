8271163778f68445a4d113d0caadc8968d3b2dc4
StLeoX <1228354389@qq.com>
2021-07-27 17:14:52 +0800

    format struct

--- misc/init.sql
+++ misc/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS lmp;
 use lmp;
-drop table if exists performance_index;
+drop table performance_index if exists performance_index;
 CREATE TABLE if NOT EXISTS performance_index (
     id int(11) AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
     plugin_name varchar(30) NOT NULL unique COMMENT '指标名',
@@ -10,14 +10,14 @@ CREATE TABLE if NOT EXISTS performance_index (
     state int NOT NULL COMMENT '状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("containerNet", "bcc", "./plugins/ContainerNet.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("cpuUtilize", "bcc", "./plugins/cpuutilize.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("hardDiskReadWriteTime", "bcc", "./plugins/harddiskreadwritetime.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("irq", "bcc", "./plugins/irq.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("memUsage", "bcc", "./plugins/memusage.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("netLatency", "bcc", "./plugins/netlatency.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("pickNext", "bcc", "./plugins/picknext.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("taskSwitch", "bcc", "./plugins/taskswitch.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("vfsStat", "bcc", "./plugins/vfsstat.py", "empty", 0);
-INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("waitingQueueLength", "bcc", "./plugins/waitingqueuelength.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("ContainerNet", "bcc", "./plugins/ContainerNet.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("CpuUtilize", "bcc", "./plugins/CpuUtilize.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("DiskReadWriteTime", "bcc", "./plugins/DiskReadWriteTime.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("Irq", "bcc", "./plugins/Irq.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("MemUsage", "bcc", "./plugins/MemUsage.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("NetworkLatency", "bcc", "./plugins/NetworkLatency.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("PickNext", "bcc", "./plugins/PickNext.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("TaskSwitch", "bcc", "./plugins/TaskSwitch.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("VfsStat", "bcc", "./plugins/VfsStat.py", "empty", 0);
+INSERT INTO performance_index(plugin_name, plugin_type, exec_path, instruction, state) VALUES("WaitingQueueLength", "bcc", "./plugins/WaitingQueueLength.py", "empty", 0);
 
