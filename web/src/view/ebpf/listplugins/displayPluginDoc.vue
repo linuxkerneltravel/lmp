44d763dac1817f3c02be0fb938269ec68d6f12cb
Chen JiFeng <chenlinuxer@163.com>
2022-03-02 06:36:33 -0800

    add plugin doc show

---
+++ web/src/view/ebpf/listplugins/displayPluginDoc.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <iframe style="width:100%;height:500px" 
+      :src="docURL" 
+      scrolling="auto" 
+      frameborder="0">
+    </iframe>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  docURL: {
+    type: String,
+    default: ''
+  },
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
