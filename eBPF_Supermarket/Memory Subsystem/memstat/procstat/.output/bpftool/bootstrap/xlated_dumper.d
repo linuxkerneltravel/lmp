e9d7248601dc12a4bdfdd1b70b90b986e39a60f6
‘unaway’ <qiaozhe1@outlook.com>
2022-07-24 16:46:44 +0800

    System/Process Memory Status Report

---
+++ eBPF_Supermarket/Memory Subsystem/memstat/procstat/.output/bpftool/bootstrap/xlated_dumper.d
@@ -0,0 +1,25 @@
+/home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/xlated_dumper.o: \
+ xlated_dumper.c \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/bpf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/types.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/bpf_common.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_common.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_version.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_legacy.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_internal.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/err.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/relo_core.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/btf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/btf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/src/kernel/bpf/disasm.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/kernel.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/build_bug.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/stringify.h \
+ json_writer.h main.h \
+ /home/qiao/libbpf-bootstrap/memstat/procstat/.output/bpftool/bootstrap/libbpf/include/bpf/hashmap.h \
+ xlated_dumper.h
---
+++ eBPF_Supermarket/Memory Subsystem/memstat/sysstat/.output/bpftool/bootstrap/btf_dumper.d
@@ -0,0 +1,19 @@
+/home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/btf_dumper.o: \
+ btf_dumper.c /home/qiao/libbpf-bootstrap/bpftool/include/linux/bitops.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/btf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/err.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/btf.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_common.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_version.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/bpf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/bpf.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/uapi/linux/bpf_common.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_legacy.h \
+ json_writer.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h main.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/kernel.h \
+ /home/qiao/libbpf-bootstrap/bpftool/include/linux/build_bug.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/hashmap.h \
+ /home/qiao/libbpf-bootstrap/memstat/sysstat/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf.h
