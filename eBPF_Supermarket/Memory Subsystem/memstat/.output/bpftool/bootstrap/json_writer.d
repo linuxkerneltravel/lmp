7f8b643108fc977f82754b96260c96ae27f09de5
xiaoxin <3174314597@qq.com>
2022-07-25 15:49:26 +0800

    readme,modify

--- eBPF_Supermarket/Memory Subsystem/memstat/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/qiao/libbpf-bootstrap/examples/memstat/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler.h \
- /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
- /home/qiao/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h
