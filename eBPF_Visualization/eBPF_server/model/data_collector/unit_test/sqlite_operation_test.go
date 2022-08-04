1dbe4a8737116ec0a6fe0a41179f23d0681f7e12
Gui-Yue <yuemeng.gui@gmail.com>
2022-08-04 22:19:49 +0800

    fix bug of sqlite write conflict.

--- eBPF_Visualization/eBPF_server/model/data_collector/unit_test/sqlite_operation_test.go
+++ eBPF_Visualization/eBPF_server/model/data_collector/unit_test/sqlite_operation_test.go
@@ -3,6 +3,7 @@ package unit_test
 import (
 	"fmt"
 	"lmp/server/model/data_collector/dao"
+	"lmp/server/model/data_collector/logic"
 	"testing"
 )
 
@@ -49,7 +50,7 @@ func TestAppendTable(t *testing.T) {
 		TableName: "plugin_test",
 	}
 	_ = ti.CreateTable()
-	if err := ti.AppendTable("TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER"); err != nil {
+	if err, _ := ti.AppendTable("TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER"); err != nil {
 		t.Error("Append plugin table failed:", err)
 	}
 }
@@ -60,9 +61,22 @@ func TestInsertRow(t *testing.T) {
 		TableName: "plugin_test",
 	}
 	_ = ti.CreateTable()
-	_ = ti.AppendTable("TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER")
+	_, _ = ti.AppendTable("TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER")
 	line := "21:52:36:      1300       16        0       13        0"
 	if err := ti.InsertRow(line); err != nil {
 		t.Error("InsertRow failed:", err)
+	}
+}
+
+func TestDataCollectorIndex(t *testing.T) {
+	if err, _ := logic.DataCollectorIndex("test_data_index", "TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER"); err != nil {
+		t.Error("DataCollectorIndex failed:", err)
+	}
+}
+
+func TestDataCollectorRow(t *testing.T) {
+	_, tableinfo := logic.DataCollectorIndex("test_data_index", "TIME|TEXT   READ_s|INTEGER WRITE_s|INTEGER FSYNC_s|INTEGER OPEN_s|INTEGER CREATE_s|INTEGER")
+	if err := logic.DataCollectorRow(tableinfo, "21:52:36:      1300       16        0       13        0"); err != nil {
+		t.Error("Insert row failed:", err)
 	}
 }
