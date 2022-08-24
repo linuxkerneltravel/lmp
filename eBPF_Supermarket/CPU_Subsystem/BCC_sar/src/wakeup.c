9e2f3f34d17a3720068aef5ccf076e035a2a5d3d
zrp <20373649@buaa.edu.cn>
2022-08-24 13:06:06 +0800

    读取PER_CPU变量成功

--- eBPF_Supermarket/CPU_Subsystem/BCC_sar/src/wakeup.c
+++ eBPF_Supermarket/CPU_Subsystem/BCC_sar/src/wakeup.c
@@ -1,6 +1,8 @@
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 
+BPF_ARRAY(symAddr, u64, 5);
+
 struct wakeup_struct {
     unsigned long long pad;
     char comm[16];
@@ -24,6 +26,17 @@ struct sleep_struct {
     u64 delay;
 };
 
+struct rq {
+	raw_spinlock_t lock;
+	unsigned int nr_running;
+	unsigned int nr_numa_running;
+	unsigned int nr_preferred_running;
+	unsigned int numa_migrate_on;
+	long unsigned int last_blocked_load_update_tick;
+	unsigned int has_blocked_load;
+};
+
+
 int trace_wakeup(struct wakeup_struct *ws) {
     char comm[16];
     struct task_struct *ts = (struct task_struct *)bpf_get_current_task();
@@ -37,7 +50,7 @@ int trace_wakeup(struct wakeup_struct *ws) {
 
 int trace_wait(struct wait_struct *ws) {
     struct task_struct *ts = (struct task_struct *)bpf_get_current_task();
-    bpf_trace_printk("pid %s-%d is waiting, current = %d\n", ws->comm, ws->pid, ts->pid);
+    // bpf_trace_printk("pid %s-%d is waiting, current = %d\n", ws->comm, ws->pid, ts->pid);
     return 0;
 }
 
@@ -48,6 +61,28 @@ int trace_block(struct block_struct *bs) {
 
 int trace_sleep(struct sleep_struct *ss) {
     struct task_struct *ts = (struct task_struct *)bpf_get_current_task();
-    bpf_trace_printk("pid %s-%d is sleeping, current = %d\n", ss->comm, ss->pid, ts->pid);
+    // bpf_trace_printk("pid %s-%d is sleeping, current = %d\n", ss->comm, ss->pid, ts->pid);
+    return 0;
+}
+
+// 在内核中，sizeof(long) == 8
+int tick_update() {
+    u32 key = 0;
+    u64 *valp, v1 = 0, v2 = 0;
+    valp = symAddr.lookup(&key);
+    if(valp) v1 = *valp;
+
+    key = 1;
+    valp = symAddr.lookup(&key);
+    if(valp) v2 = *valp;
+
+    u64 percpu_offset[2];
+    struct rq _rq;
+    bpf_probe_read_kernel(percpu_offset, sizeof(u64) * 2, v1);
+    u64 rq_ptr = v2 + percpu_offset[0];
+    bpf_probe_read_kernel(&_rq, sizeof(struct rq), rq_ptr);
+
+    // bpf_trace_printk("%lx %lx\n", percpu_offset[0], percpu_offset[1]);
+    bpf_trace_printk("rqlen = %d\n", _rq.nr_running);
     return 0;
 }
--- eBPF_Supermarket/CPU_Subsystem/BCC_sar/src/wakeup.py
+++ eBPF_Supermarket/CPU_Subsystem/BCC_sar/src/wakeup.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 from time import sleep
-from bcc import BPF
+from bcc import BPF, PerfType, PerfSWConfig
 import bpfutil, ctypes
 
 bpf = BPF(src_file="wakeup.c")
@@ -10,13 +10,25 @@ bpf.attach_tracepoint(tp="sched:sched_wakeup", fn_name="trace_wakeup")
 bpf.attach_tracepoint(tp="sched:sched_process_wait", fn_name="trace_wait")
 bpf.attach_tracepoint(tp="sched:sched_stat_blocked",fn_name="trace_block")
 
+# 传递给bpf程序的定时事件
+bpf.attach_perf_event(ev_type=PerfType.SOFTWARE,
+    ev_config=PerfSWConfig.CPU_CLOCK,
+    fn_name="tick_update",
+    sample_period=0, 
+    sample_freq=3
+)
+
 # 内核中的重要变量定义，留作参考，勿删！
 # pcpu_base_addr = int(bpfutil.find_ksym("pcpu_base_addr"), base=16) # u32, Read Only
 # __per_cpu_start = int(bpfutil.find_ksym("__per_cpu_start"), base=16) # u32, Read Only
 # pcpu_unit_offsets = int(bpfutil.find_ksym("pcpu_unit_offsets"), base=16) # u32 array, Read Only
 
 __per_cpu_offset = int(bpfutil.find_ksym("__per_cpu_offset"), base=16) # u32 array, Inited
-runqueues = int(bpfutil.find_ksym("runqueues"), base=16) # per_cpu var
+runqueues = int(bpfutil.find_ksym("runqueues"), base=16) # per_cpu var, u32
+
+bpf['symAddr'][0] = ctypes.c_ulonglong(__per_cpu_offset)
+bpf['symAddr'][1] = ctypes.c_ulonglong(runqueues)
+# print(bpfutil.find_ksym("__per_cpu_offset"))
 # 计算方式：ptr_to_PERCPU_var + __per_cpu_offset[cpu]
 
 while 1:
