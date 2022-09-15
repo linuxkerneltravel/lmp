7dae94a7ee208ff98e457413282850b4a314a8df
Woa <0903wzy@gmail.com>
2022-09-15 12:21:26 +0800

    refactor(sidecar): change module name

--- eBPF_Supermarket/sidecar/perf/net/stack.go
+++ eBPF_Supermarket/sidecar/perf/net/stack.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/opentracing/opentracing-go"
 
-	"github.com/eswzy/podstat/bpf/podnet"
-	"github.com/eswzy/podstat/visualization"
+	"github.com/linuxkerneltravel/lmp/eBPF_Supermarket/sidecar/bpf/podnet"
+	"github.com/linuxkerneltravel/lmp/eBPF_Supermarket/sidecar/visualization"
 )
 
 var TimeOffset = time.Now()
