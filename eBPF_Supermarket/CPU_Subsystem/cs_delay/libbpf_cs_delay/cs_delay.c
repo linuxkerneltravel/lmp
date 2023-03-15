bbcb295f0bc2db13c33d488e06a85022506bc681
zhangzihengya <117833688+zhangzihengya@users.noreply.github.com>
2023-03-15 20:09:42 +0800

    Update cs_delay.c

--- eBPF_Supermarket/CPU_Subsystem/cs_delay/libbpf_cs_delay/cs_delay.c
+++ eBPF_Supermarket/CPU_Subsystem/cs_delay/libbpf_cs_delay/cs_delay.c
@@ -1,6 +1,6 @@
 /* 
-    ×¢Òâ£ºÊ¹ÓÃ¸Ã³ÌĞòÊ±,½«µÚ94ĞĞ´úÂë×¢ÊÍµô 
-    Ô­Òò£º¸ÃĞĞ´úÂëÎªÁËÄÜÊ¹³ÌĞòÔËĞĞ½áÊø£¬³É¹¦Ìá½»ÖÁGithub²Ö¿â 
+    æ³¨æ„ï¼šä½¿ç”¨è¯¥ç¨‹åºæ—¶,å°†ç¬¬94è¡Œä»£ç æ³¨é‡Šæ‰ 
+    åŸå› ï¼šè¯¥è¡Œä»£ç ä¸ºäº†èƒ½ä½¿ç¨‹åºè¿è¡Œç»“æŸï¼ŒæˆåŠŸæäº¤è‡³Githubä»“åº“ 
 */ 
 
 #include <stdio.h>
@@ -8,8 +8,7 @@
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include <signal.h>
-#include "cs_delay.skel.h"	//°üº¬ÁË BPF ×Ö½ÚÂëºÍÏà¹ØµÄ¹ÜÀíº¯Êı
-#include "cs_delay.h"
+#include "cs_delay.skel.h"	//åŒ…å«äº† BPF å­—èŠ‚ç å’Œç›¸å…³çš„ç®¡ç†å‡½æ•°
 
 static volatile bool exiting = false;
 
@@ -39,48 +38,48 @@ int main(int argc, char **argv)
 	int err;
 
 	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-	/* ÉèÖÃlibbpf´íÎóºÍµ÷ÊÔĞÅÏ¢»Øµ÷ */
+	/* è®¾ç½®libbpfé”™è¯¯å’Œè°ƒè¯•ä¿¡æ¯å›è°ƒ */
 	libbpf_set_print(libbpf_print_fn);
 
-	/* ¸ü¸É¾»µØ´¦ÀíCtrl-C
-	   SIGINT£ºÓÉInterrupt Key²úÉú£¬Í¨³£ÊÇCTRL+C»òÕßDELETE¡£·¢ËÍ¸øËùÓĞForeGround GroupµÄ½ø³Ì
-       SIGTERM£ºÇëÇóÖĞÖ¹½ø³Ì£¬killÃüÁî·¢ËÍ
+	/* æ›´å¹²å‡€åœ°å¤„ç†Ctrl-C
+	   SIGINTï¼šç”±Interrupt Keyäº§ç”Ÿï¼Œé€šå¸¸æ˜¯CTRL+Cæˆ–è€…DELETEã€‚å‘é€ç»™æ‰€æœ‰ForeGround Groupçš„è¿›ç¨‹
+       SIGTERMï¼šè¯·æ±‚ä¸­æ­¢è¿›ç¨‹ï¼Œkillå‘½ä»¤å‘é€
 	*/
-	signal(SIGINT, sig_handler);		//signalÉèÖÃÄ³Ò»ĞÅºÅµÄ¶ÔÓ¦¶¯×÷
+	signal(SIGINT, sig_handler);		//signalè®¾ç½®æŸä¸€ä¿¡å·çš„å¯¹åº”åŠ¨ä½œ
 	signal(SIGTERM, sig_handler);
 
-	/* ´ò¿ªBPFÓ¦ÓÃ³ÌĞò */
+	/* æ‰“å¼€BPFåº”ç”¨ç¨‹åº */
 	skel = cs_delay_bpf__open();
 	if (!skel) {
 		fprintf(stderr, "Failed to open BPF skeleton\n");
 		return 1;
 	}
 	
-	/* ¼ÓÔØ²¢ÑéÖ¤BPF³ÌĞò */
+	/* åŠ è½½å¹¶éªŒè¯BPFç¨‹åº */
 	err = cs_delay_bpf__load(skel);
 	if (err) {
 		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
 		goto cleanup;
 	}
 	
-	/* ¸½¼Ó¸ú×Ùµã´¦Àí³ÌĞò */
+	/* é™„åŠ è·Ÿè¸ªç‚¹å¤„ç†ç¨‹åº */
 	err = cs_delay_bpf__attach(skel);
 	if (err) {
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
 	}
 	
-	/* ÉèÖÃ»·ĞÎ»º³åÇøÂÖÑ¯ */
-	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);	//ring_buffer__new() API£¬ÔÊĞíÔÚ²»Ê¹ÓÃ¶îÍâÑ¡ÏîÊı¾İ½á¹¹ÏÂÖ¸¶¨»Øµ÷
+	/* è®¾ç½®ç¯å½¢ç¼“å†²åŒºè½®è¯¢ */
+	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);	//ring_buffer__new() APIï¼Œå…è®¸åœ¨ä¸ä½¿ç”¨é¢å¤–é€‰é¡¹æ•°æ®ç»“æ„ä¸‹æŒ‡å®šå›è°ƒ
 	if (!rb) {
 		err = -1;
 		fprintf(stderr, "Failed to create ring buffer\n");
 		goto cleanup;
 	}
 	
-	/* ´¦ÀíÊÂ¼ş */
+	/* å¤„ç†äº‹ä»¶ */
 	while (!exiting) {
-		err = ring_buffer__poll(rb, 100 /* timeout, ms */);		//ring_buffer__poll(),ÂÖÑ¯´ò¿ªringbuf»º³åÇø¡£Èç¹ûÓĞÊÂ¼ş£¬handle_eventº¯Êı»áÖ´ĞĞ
+		err = ring_buffer__poll(rb, 100 /* timeout, ms */);		//ring_buffer__poll(),è½®è¯¢æ‰“å¼€ringbufç¼“å†²åŒºã€‚å¦‚æœæœ‰äº‹ä»¶ï¼Œhandle_eventå‡½æ•°ä¼šæ‰§è¡Œ
 		/* Ctrl-C will cause -EINTR */
 		if (err == -EINTR) {
 			err = 0;
@@ -91,11 +90,11 @@ int main(int argc, char **argv)
 			break;
 		}
 		
-		exiting = true;			//Ê¹ÓÃ¸Ã³ÌĞòÊ±,½«¸ÃĞĞ´úÂë×¢ÊÍµô 
+		exiting = true;			//ä½¿ç”¨è¯¥ç¨‹åºæ—¶,å°†è¯¥è¡Œä»£ç æ³¨é‡Šæ‰ 
 		
 	}
 	
-/* Ğ¶ÔØBPF³ÌĞò */
+/* å¸è½½BPFç¨‹åº */
 cleanup:
 	ring_buffer__free(rb);
 	cs_delay_bpf__destroy(skel);
