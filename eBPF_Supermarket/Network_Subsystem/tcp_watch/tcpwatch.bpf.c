830911fe9cfdd902ab9c57d369c3906c9d36b709
Chui <blown.away@qq.com>
2023-07-21 19:20:58 +0800

    merge the delay_contact code into tcpwatch

--- eBPF_Supermarket/Network_Subsystem/tcp_watch/tcpwatch.bpf.c
+++ eBPF_Supermarket/Network_Subsystem/tcp_watch/tcpwatch.bpf.c
@@ -28,6 +28,20 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
     __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, MAX_CONN *MAX_PACKET);
+    __type(key, struct packet_tuple);
+    __type(value, struct ktime_info);
+} timestamps SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, MAX_CONN);
+    __type(key, struct sock *);
+    __type(value, struct packs_lru_buf_t); // fd of map
+} packets_info SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
     __uint(max_entries, MAX_CONN);
     __type(key, struct sock *);
     __type(value, struct conn_t);
@@ -56,9 +70,54 @@ const volatile int filter_sport = 0;
         }                                                                      \
     }
 
+static struct tcp_sock *tcp_sk(const struct sock *sk) {
+    return (struct tcp_sock *)sk;
+}
+
 static struct tcphdr *skb_to_tcphdr(const struct sk_buff *skb) {
     return (struct tcphdr *)((BPF_CORE_READ(skb, head) +
                               BPF_CORE_READ(skb, transport_header)));
+}
+
+static inline struct iphdr *skb_to_iphdr(const struct sk_buff *skb) {
+    return (struct iphdr *)(BPF_CORE_READ(skb, head) +
+                            BPF_CORE_READ(skb, network_header));
+}
+
+static inline struct ipv6hdr *skb_to_ipv6hdr(const struct sk_buff *skb) {
+    return (struct ipv6hdr *)(BPF_CORE_READ(skb, head) +
+                              BPF_CORE_READ(skb, network_header));
+}
+
+static void get_pkt_tuple(struct packet_tuple *pkt_tuple, struct iphdr *ip,
+                          struct tcphdr *tcp) {
+    pkt_tuple->saddr = BPF_CORE_READ(ip, saddr);
+    pkt_tuple->daddr = BPF_CORE_READ(ip, daddr);
+    u16 sport = BPF_CORE_READ(tcp, source);
+    u16 dport = BPF_CORE_READ(tcp, dest);
+    pkt_tuple->sport = __bpf_ntohs(sport);
+    pkt_tuple->dport = __bpf_ntohs(dport);
+    u32 seq = BPF_CORE_READ(tcp, seq);
+    u32 ack = BPF_CORE_READ(tcp, ack_seq);
+    pkt_tuple->seq = __bpf_ntohl(seq);
+    pkt_tuple->ack = __bpf_ntohl(ack);
+}
+
+static void get_pkt_tuple_v6(struct packet_tuple *pkt_tuple,
+                             struct ipv6hdr *ip6h, struct tcphdr *tcp) {
+    bpf_probe_read_kernel(&pkt_tuple->saddr_v6, sizeof(pkt_tuple->saddr_v6),
+                          &ip6h->saddr.in6_u.u6_addr32);
+    bpf_probe_read_kernel(&pkt_tuple->daddr_v6, sizeof(pkt_tuple->daddr_v6),
+                          &ip6h->daddr.in6_u.u6_addr32);
+    u16 sport = BPF_CORE_READ(tcp, source);
+    u16 dport = BPF_CORE_READ(tcp, dest);
+    pkt_tuple->sport = __bpf_ntohs(sport);
+    pkt_tuple->dport = __bpf_ntohs(dport);
+
+    u32 seq = BPF_CORE_READ(tcp, seq);
+    u32 ack = BPF_CORE_READ(tcp, ack_seq);
+    pkt_tuple->seq = __bpf_ntohl(seq);
+    pkt_tuple->ack = __bpf_ntohl(ack);
 }
 
 /**
@@ -103,6 +162,7 @@ int BPF_KRETPROBE(inet_csk_accept, struct sock *newsk) {
     }
 
     conn.init_timestamp = bpf_ktime_get_ns();
+    conn.ptid = bpf_get_current_pid_tgid();
 
     int err = bpf_map_update_elem(&conns_info, &newsk, &conn, BPF_ANY);
     if (err) {
@@ -144,7 +204,7 @@ int BPF_KRETPROBE(tcp_v4_connect_exit, int ret) {
     struct sock *sk = *skp;
     struct conn_t conn = {};
     conn.sock = sk;
-
+    conn.ptid = pid;
     u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
     __be16 dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
     u16 sport = BPF_CORE_READ(sk, __sk_common.skc_num);
@@ -197,7 +257,7 @@ int BPF_KRETPROBE(tcp_v6_connect_exit, int ret) {
     struct sock *sk = *skp;
     struct conn_t conn = {};
     conn.sock = sk;
-
+    conn.ptid = pid;
     u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
     __be16 dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
     u16 sport = BPF_CORE_READ(sk, __sk_common.skc_num);
@@ -226,3 +286,466 @@ int BPF_KRETPROBE(tcp_v6_connect_exit, int ret) {
 
     return 0;
 }
+
+/* TCP State */
+SEC("kprobe/tcp_set_state")
+int BPF_KPROBE(tcp_set_state, struct sock *sk, int state) {
+    struct conn_t *value = bpf_map_lookup_elem(&conns_info, &sk);
+    if (state == TCP_CLOSE && value) {
+        // delete
+        bpf_map_delete_elem(&sock_stores, &value->ptid);
+        bpf_map_delete_elem(&conns_info, &sk);
+    }
+    return 0;
+}
+
+/*!
+in_ipv4:
+    kprobe/eth_type_trans
+    kprobe/ip_rcv_core.isra.0
+    kprobe/tcp_v4_rcv
+    kprobe/skb_copy_datagram_iter
+
+in_ipv6:
+    kprobe/eth_type_trans
+    kprobe/ip6_rcv_core.isra.0
+    kprobe/tcp_v6_rcv
+    kprobe/skb_copy_datagram_iter
+
+out_ipv4:
+    kprobe/tcp_sendmsg
+    kprobe/ip_queue_xmit
+    kprobe/dev_queue_xmit
+    kprobe/dev_hard_start_xmit
+
+out_ipv6:
+    kprobe/tcp_sendmsg
+    kprobe/inet6_csk_xmit
+    kprobe/dev_queue_xmit
+    kprobe/dev_hard_start_xmit
+
+*/
+/************************************************ receive path
+ * ****************************************/
+/** in ipv4 && ipv6 */
+SEC("kprobe/eth_type_trans")
+int BPF_KPROBE(eth_type_trans, struct sk_buff *skb) {
+    const struct ethhdr *eth = (struct ethhdr *)BPF_CORE_READ(skb, data);
+    u16 protocol = BPF_CORE_READ(eth, h_proto);
+
+    if (protocol == __bpf_ntohs(ETH_P_IP)) { // Protocol is IP
+        struct iphdr *ip = (struct iphdr *)(BPF_CORE_READ(skb, data) + 14);
+        struct tcphdr *tcp = (struct tcphdr *)(BPF_CORE_READ(skb, data) +
+                                               sizeof(struct iphdr) + 14);
+        struct packet_tuple pkt_tuple = {};
+        get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        struct ktime_info *tinfo, zero = {};
+
+        tinfo = (struct ktime_info *)bpf_map_lookup_or_try_init(
+            &timestamps, &pkt_tuple, &zero);
+        if (tinfo == NULL) {
+            return 0;
+        }
+        tinfo->mac_time = bpf_ktime_get_ns() / 1000;
+    } else if (protocol == __bpf_htons(ETH_P_IPV6)) { // Protocol is IPV6
+        struct ipv6hdr *ip6h =
+            (struct ipv6hdr *)(BPF_CORE_READ(skb, data) + 14);
+        struct tcphdr *tcp = (struct tcphdr *)(BPF_CORE_READ(skb, data) +
+                                               sizeof(struct ipv6hdr) + 14);
+        struct packet_tuple pkt_tuple = {};
+        get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        struct ktime_info *tinfo, zero = {};
+
+        tinfo = (struct ktime_info *)bpf_map_lookup_or_try_init(
+            &timestamps, &pkt_tuple, &zero);
+        if (tinfo == NULL) {
+            return 0;
+        }
+        tinfo->mac_time = bpf_ktime_get_ns();
+    }
+    return 0;
+}
+
+/** in only ipv4 */
+SEC("kprobe/ip_rcv_core")
+int BPF_KPROBE(ip_rcv_core, struct sk_buff *skb) {
+
+    if (skb == NULL)
+        return 0;
+    struct iphdr *ip = skb_to_iphdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+    // FILTER_DPORT
+    // FILTER_SPORT
+
+    struct ktime_info *tinfo;
+    if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+        return 0;
+    }
+    tinfo->ip_time = bpf_ktime_get_ns();
+
+    return 0;
+}
+
+/** in only ipv6 */
+SEC("kprobe/ip6_rcv_core")
+int BPF_KPROBE(ip6_rcv_core, struct sk_buff *skb) {
+
+    if (skb == NULL)
+        return 0;
+    struct ipv6hdr *ip6h = skb_to_ipv6hdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+    // FILTER_DPORT
+    // FILTER_SPORT
+
+    struct ktime_info *tinfo;
+    if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+        return 0;
+    }
+    tinfo->ip_time = bpf_ktime_get_ns();
+
+    return 0;
+}
+
+/**in only ipv4 */
+SEC("kprobe/tcp_v4_rcv")
+int BPF_KPROBE(tcp_v4_rcv, struct sk_buff *skb) {
+
+    if (skb == NULL)
+        return 0;
+    struct iphdr *ip = skb_to_iphdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+    // FILTER_DPORT
+    // FILTER_SPORT
+
+    struct ktime_info *tinfo;
+    if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+        return 0;
+    }
+    tinfo->tcp_time = bpf_ktime_get_ns();
+
+    return 0;
+}
+
+/** in only ipv6 */
+SEC("kprobe/tcp_v6_rcv")
+int BPF_KPROBE(tcp_v6_rcv, struct sk_buff *skb) {
+
+    if (skb == NULL)
+        return 0;
+    struct ipv6hdr *ip6h = skb_to_ipv6hdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+    // FILTER_DPORT
+    // FILTER_SPORT
+
+    struct ktime_info *tinfo;
+    if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+        return 0;
+    }
+    tinfo->tcp_time = bpf_ktime_get_ns();
+
+    return 0;
+}
+
+// v4 & v6 do_rcv to get sk and other info
+
+/** in ipv4 && ipv6 */
+SEC("kprobe/skb_copy_datagram_iter")
+int BPF_KPROBE(skb_copy_datagram_iter, struct sk_buff *skb) {
+    const struct ethhdr *eth = (struct ethhdr *)BPF_CORE_READ(skb, data);
+    u16 protocol = BPF_CORE_READ(eth, h_proto);
+
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    struct ktime_info *tinfo;
+
+    if (protocol == __bpf_ntohs(ETH_P_IP)) { /** ipv4 */
+        if (skb == NULL)
+            return 0;
+        struct iphdr *ip = skb_to_iphdr(skb);
+        get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->app_time = bpf_ktime_get_ns();
+    } else if (protocol == __bpf_htons(ETH_P_IPV6)) {
+        if (skb == NULL)
+            return 0;
+        struct ipv6hdr *ip6h = skb_to_ipv6hdr(skb);
+        get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->app_time = bpf_ktime_get_ns();
+    }
+
+    return 0;
+}
+
+/***************************************** end of receive path
+ * ****************************************/
+
+// SRTT tcp_ack
+
+/************************************************ send path
+ * *******************************************/
+/*!
+ * \brief: 获取数据包进入TCP层时刻的时间戳, 发送tcp层起始点
+ *         out ipv4 && ipv6
+ */
+SEC("kprobe/tcp_sendmsg")
+int BPF_KPROBE(tcp_sendmsg, struct sock *sk, struct msghdr *msg, size_t size) {
+
+    u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
+    struct ktime_info *tinfo, zero = {};
+    struct packet_tuple pkt_tuple = {};
+    /** ipv4 */
+    if (family == AF_INET) {
+        u16 dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+        pkt_tuple.saddr = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
+        pkt_tuple.daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+        pkt_tuple.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        pkt_tuple.dport = __bpf_ntohs(dport);
+
+        u32 snd_nxt = BPF_CORE_READ(tcp_sk(sk), snd_nxt);
+        u32 rcv_nxt = BPF_CORE_READ(tcp_sk(sk), rcv_nxt);
+        pkt_tuple.seq = snd_nxt;
+        pkt_tuple.ack = rcv_nxt;
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        tinfo = (struct ktime_info *)bpf_map_lookup_or_try_init(
+            &timestamps, &pkt_tuple, &zero);
+        if (tinfo == NULL) {
+            return 0;
+        }
+        tinfo->tcp_time = bpf_ktime_get_ns();
+    } else if (family == AF_INET6) {
+        bpf_probe_read_kernel(
+            &pkt_tuple.saddr_v6,
+            sizeof(sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32),
+            &sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+
+        bpf_probe_read_kernel(
+            &pkt_tuple.daddr_v6,
+            sizeof(sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32),
+            &sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
+
+        pkt_tuple.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        u16 dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+        pkt_tuple.dport = __bpf_ntohs(dport);
+
+        u32 snd_nxt = BPF_CORE_READ(tcp_sk(sk), snd_nxt);
+        u32 rcv_nxt = BPF_CORE_READ(tcp_sk(sk), rcv_nxt);
+        pkt_tuple.seq = snd_nxt;
+        pkt_tuple.ack = rcv_nxt;
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        tinfo = (struct ktime_info *)bpf_map_lookup_or_try_init(
+            &timestamps, &pkt_tuple, &zero);
+        if (tinfo == NULL) {
+            return 0;
+        }
+        tinfo->tcp_time = bpf_ktime_get_ns();
+    }
+
+    return 0;
+}
+
+/*!
+* \brief: 获取数据包进入IP层时刻的时间戳
+* tips:   此时ip数据段还没有数据，不能用 get_pkt_tuple(&pkt_tuple, ip,
+tcp)获取ip段的数据 out only ipv4
+*/
+SEC("kprobe/ip_queue_xmit")
+int BPF_KPROBE(ip_queue_xmit, struct sock *sk, struct sk_buff *skb) {
+    u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
+    if (family == AF_INET) {
+        struct packet_tuple pkt_tuple = {};
+        struct tcphdr *tcp = skb_to_tcphdr(skb);
+        u16 dport;
+        u32 seq, ack;
+        pkt_tuple.saddr = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
+        pkt_tuple.daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+        pkt_tuple.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+        pkt_tuple.dport = __bpf_ntohs(dport);
+        seq = BPF_CORE_READ(tcp, seq);
+        ack = BPF_CORE_READ(tcp, ack_seq);
+        pkt_tuple.seq = __bpf_ntohl(seq);
+        pkt_tuple.ack = __bpf_ntohl(ack);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        struct ktime_info *tinfo;
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            // debug info look : cat
+            // /sys/kernel/debug/tracing/trace_pipe, root mode
+            __bpf_printk("Hash search failed, please check!\n");
+            return 0;
+        }
+        tinfo->ip_time = bpf_ktime_get_ns();
+    }
+    return 0;
+};
+
+/*!
+* \brief: 获取数据包进入IP层时刻的时间戳
+* tips:   此时ip数据段还没有数据，不能用 get_pkt_tuple(&pkt_tuple, ip,
+tcp)获取ip段的数据 out only ipv6
+*/
+SEC("kprobe/inet6_csk_xmit")
+int BPF_KPROBE(inet6_csk_xmit, struct sock *sk, struct sk_buff *skb) {
+    u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
+    if (family == AF_INET6) {
+        struct packet_tuple pkt_tuple = {};
+        struct tcphdr *tcp = skb_to_tcphdr(skb);
+        u16 dport;
+        u32 seq, ack;
+
+        bpf_probe_read_kernel(
+            &pkt_tuple.saddr_v6,
+            sizeof(sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32),
+            &sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+
+        bpf_probe_read_kernel(
+            &pkt_tuple.daddr_v6,
+            sizeof(sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32),
+            &sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
+
+        pkt_tuple.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+        pkt_tuple.dport = __bpf_ntohs(dport);
+        seq = BPF_CORE_READ(tcp, seq);
+        ack = BPF_CORE_READ(tcp, ack_seq);
+        pkt_tuple.seq = __bpf_ntohl(seq);
+        pkt_tuple.ack = __bpf_ntohl(ack);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        struct ktime_info *tinfo;
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            // debug info look : cat
+            // /sys/kernel/debug/tracing/trace_pipe, root mode
+            __bpf_printk("Hash search failed, please check!\n");
+            return 0;
+        }
+        tinfo->ip_time = bpf_ktime_get_ns();
+    }
+
+    return 0;
+};
+
+/*!
+* \brief: 获取数据包进入数据链路层时刻的时间戳
+    out ipv4 && ipv6
+*/
+SEC("kprobe/__dev_queue_xmit")
+int BPF_KPROBE(__dev_queue_xmit, struct sk_buff *skb) {
+    const struct ethhdr *eth = (struct ethhdr *)BPF_CORE_READ(skb, data);
+    u16 protocol = BPF_CORE_READ(eth, h_proto);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    struct ktime_info *tinfo;
+    if (protocol == __bpf_ntohs(ETH_P_IP)) {
+        /** ipv4 */
+        struct iphdr *ip = skb_to_iphdr(skb);
+        get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->mac_time = bpf_ktime_get_ns();
+    } else if (protocol == __bpf_ntohs(ETH_P_IPV6)) {
+        /** ipv6 */
+        struct ipv6hdr *ip6h = skb_to_ipv6hdr(skb);
+        get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->mac_time = bpf_ktime_get_ns();
+    }
+
+    return 0;
+};
+
+/*!
+* \brief: 获取数据包发送时刻的时间戳
+    out ipv4 && ipv6
+*/
+SEC("kprobe/dev_hard_start_xmit")
+int BPF_KPROBE(dev_hard_start_xmit, struct sk_buff *skb) {
+    const struct ethhdr *eth = (struct ethhdr *)BPF_CORE_READ(skb, data);
+    u16 protocol = BPF_CORE_READ(eth, h_proto);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    struct packet_tuple pkt_tuple = {};
+    struct ktime_info *tinfo;
+    if (protocol == __bpf_ntohs(ETH_P_IP)) {
+        /** ipv4 */
+        struct iphdr *ip = skb_to_iphdr(skb);
+        get_pkt_tuple(&pkt_tuple, ip, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->qdisc_time = bpf_ktime_get_ns();
+    } else if (protocol == __bpf_ntohs(ETH_P_IPV6)) {
+        /** ipv6 */
+        struct ipv6hdr *ip6h = skb_to_ipv6hdr(skb);
+        get_pkt_tuple_v6(&pkt_tuple, ip6h, tcp);
+
+        // FILTER_DPORT
+        // FILTER_SPORT
+
+        if ((tinfo = bpf_map_lookup_elem(&timestamps, &pkt_tuple)) == NULL) {
+            return 0;
+        }
+        tinfo->qdisc_time = bpf_ktime_get_ns();
+    }
+    u16 nat_sport = 0;
+    nat_sport = BPF_CORE_READ(tcp, source);
+
+    return 0;
+};
--- eBPF_Supermarket/Network_Subsystem/tcp_watch/tcpwatch.h
+++ eBPF_Supermarket/Network_Subsystem/tcp_watch/tcpwatch.h
@@ -1,6 +1,10 @@
 #ifndef __TCPWATCH_H
 #define __TCPWATCH_H
 
+// more info ： include/uapi/linux/if_ether.h
+#define ETH_P_IP	0x0800		/* Internet Protocol packet	*/
+#define ETH_P_IPV6	0x86DD		/* IPv6 over bluebook		*/
+
 #ifndef AF_INET
 #define AF_INET 2
 #endif
@@ -11,6 +15,7 @@
 
 struct conn_t {
     const void * sock;
+    unsigned int ptid;
     unsigned short family;
     unsigned __int128 saddr_v6;
     unsigned __int128 daddr_v6;
@@ -24,5 +29,42 @@ struct conn_t {
     unsigned long long init_timestamp;
     unsigned long long  duration;
 };
+
+#define MAX_PACKET 1000
+
+struct packet_tuple {
+    unsigned __int128 saddr_v6;
+    unsigned __int128 daddr_v6;
+    unsigned int saddr;
+    unsigned int daddr;
+    unsigned short sport;
+    unsigned short dport;
+    unsigned int seq;
+    unsigned int ack;
+};
+
+struct ktime_info { // us time stamp info
+    unsigned long long qdisc_time;
+    unsigned long long mac_time;
+    unsigned long long ip_time;
+    unsigned long long tcp_time;
+    unsigned long long app_time;
+    unsigned int srtt;
+};
+
+struct pack_t{ // us time duration info
+    long mac_time;
+    long ip_time;
+    long tcp_time;
+    long seq;
+    long ack;
+};
+
+struct packs_lru_buf_t {
+    struct pack_t packets[MAX_PACKET];
+    int begin_index;
+    int end_index;
+};
+
 
 #endif /* __TCPWATCH_H */
