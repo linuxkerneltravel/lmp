e4e76a7effdf706fb366afa115a88c587f77e4ab
zhangzihengya <117833688+zhangzihengya@users.noreply.github.com>
2023-07-27 17:36:01 +0800

    Update mutex_test1.c

--- eBPF_Supermarket/eBPF_proc_image/docs/mutex/mutex_test1.c
+++ eBPF_Supermarket/eBPF_proc_image/docs/mutex/mutex_test1.c
@@ -11,28 +11,6 @@ MODULE_LICENSE("GPL");
 
 static struct mutex mutex;
 
-/*
-static void write_hello_world(void) {
-    struct file *file;
-    ssize_t ret;
-    const char *filename = "test.txt";
-    const char *data = "hello world\n";
-
-    file = filp_open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (IS_ERR(file)) {
-        printk(KERN_ERR "Error opening file %s\n", filename);
-        return;
-    }
-
-    ret = kernel_write(file, data, strlen(data), &file->f_pos);
-    if (ret < 0) {
-        printk(KERN_ERR "Error writing to file %s\n", filename);
-    }
-
-    filp_close(file, NULL);
-}
-*/
-
 static int test_init(void) {
     pid_t pid = current->pid;
     printk(KERN_INFO "Hello from kernel module. PID: %d\n", pid);
@@ -40,7 +18,6 @@ static int test_init(void) {
     ssleep(15);                 //为了在持有锁之前读取进程pid，以便对其进行跟踪
 
     mutex_lock(&mutex);
-    //write_hello_world();
     ssleep(3); 
     mutex_unlock(&mutex);
 
