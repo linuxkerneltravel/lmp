476efeac25e0ca30553788987bc00a6115f11cec
LiuLingze <luiyanbing@foxmail.com>
2023-07-28 14:19:57 +0000

    添加注释

--- eBPF_Supermarket/Stack_Analyser/libbpf/bpf/on_cpu_count.bpf.c
+++ eBPF_Supermarket/Stack_Analyser/libbpf/bpf/on_cpu_count.bpf.c
@@ -1,3 +1,21 @@
+// Copyright 2023 The LMP Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// author: luiyanbing@foxmail.com
+//
+// 内核态bpf的on-cpu模块代码
+
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
@@ -12,12 +30,12 @@ BPF_STACK_TRACE(stack_trace);
 BPF_HASH(psid_count, psid, u32);
 BPF_HASH(pid_comm, u32, comm);
 
-char u,k;
+char u /*user stack flag*/, k /*kernel stack flag*/; 
 
 SEC("perf_event")
 int do_stack(void *ctx)
 {
-    bpf_printk("");
+    // record data
     struct task_struct *curr = (void *)bpf_get_current_task();
     u32 pid = BPF_CORE_READ(curr, pid);
     u32 tgid = BPF_CORE_READ(curr, tgid);
@@ -34,6 +52,8 @@ int do_stack(void *ctx)
         .usid = u?USER_STACK:-1,
         .ksid = k?KERNEL_STACK:-1,
     };
+
+    // add cosunt
     u32 *count = bpf_map_lookup_elem(&psid_count, &apsid);
     if (count)
         (*count)++;
