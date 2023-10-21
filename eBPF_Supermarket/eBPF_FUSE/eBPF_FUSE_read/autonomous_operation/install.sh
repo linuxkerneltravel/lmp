5307256015a85f8124ee0f632655138531dff940
13186379707 <13186379707@163.com>
2023-10-21 18:43:00 +0800

    Delete eBPF_Supermarket/eBPF_FUSE/eBPF_FUSE_read/autonomous_operation directory

--- eBPF_Supermarket/eBPF_FUSE/eBPF_FUSE_read/autonomous_operation/install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install make gcc bc -y &&
-sudo apt install pkg-config libncurses-dev -y &&
-sudo apt install flex -y &&
-sudo apt install bison -y &&
-sudo apt install -y libncurses5-dev flex bison libssl-dev dpkg-dev libelf-dev&&
-sudo apt install -y wget curl &&
-sudo apt install fakeroot -y&&
-sudo apt install rsync kmod cpio -y
