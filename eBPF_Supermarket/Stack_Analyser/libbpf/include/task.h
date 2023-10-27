0b01dc1781999ae58fb67f34dfc641512f34460d
LiuLingze <luiyanbing@foxmail.com>
2023-10-27 04:02:13 +0000

    添加了文件头
    Signed-off-by: LiuLingze <luiyanbing@foxmail.com>

--- eBPF_Supermarket/Stack_Analyser/libbpf/include/task.h
+++ eBPF_Supermarket/Stack_Analyser/libbpf/include/task.h
@@ -1,3 +1,19 @@
+// Copyright 2019 Aqua Security Software Ltd. 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This product includes software developed by Aqua Security (https://aquasec.com).
+
 #ifndef __COMMON_TASK_H__
 #define __COMMON_TASK_H__
 
