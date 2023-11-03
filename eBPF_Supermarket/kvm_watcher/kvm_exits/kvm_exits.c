90aa90ad906c4ca8f987a355fac9106fe0cf3118
nanshuaibo <nanshuaibo811@163.com>
2023-11-03 19:17:26 +0800

    添加文件头信息

--- eBPF_Supermarket/kvm_watcher/kvm_exits/kvm_exits.c
+++ eBPF_Supermarket/kvm_watcher/kvm_exits/kvm_exits.c
@@ -1,3 +1,20 @@
+// Copyright 2023 The LMP Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// author: nanshuaibo811@163.com
+//
+// User space BPF program used for outputting VM exit reason.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -126,9 +143,6 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Failed to open BPF skeleton\n");
 		return 1;
 	}
-
-	/* ensure BPF program only handles write() syscalls from our process */
-	//skel->bss->my_pid = getpid();
 
 	/* Load & verify BPF programs */
 	err = kvm_exits_bpf__load(skel);
