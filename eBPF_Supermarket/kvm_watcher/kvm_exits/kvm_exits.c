1a61d23792d979ad88689212a7ce09baa2feef15
nanshuaibo <nanshuaibo811@163.com>
2023-11-10 15:51:04 +0800

    优化结果输出

--- eBPF_Supermarket/kvm_watcher/kvm_exits/kvm_exits.c
+++ eBPF_Supermarket/kvm_watcher/kvm_exits/kvm_exits.c
@@ -18,14 +18,18 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/resource.h>
 #include <time.h>
+#include <argp.h>
 #include <bpf/libbpf.h>
 #include "kvm_exits.skel.h"
 #include "kvm_exits.h"
 
-// 存储所有退出原因的映射关系
+
+// 存储所有exit reason的映射关系
 // from arch/x86/include/uapi/asm/vmx.h
 struct ExitReason exitReasons[] = {
     {0, "EXCEPTION_NMI"},
@@ -102,6 +106,163 @@ const char* getExitReasonName(int number) {
     return "Unknown"; // 如果找不到对应的退出原因，返回一个默认值
 }
 
+typedef struct {
+    int exit_reason;
+    char info[256]; // 替换成适当的大小
+	unsigned long long total_dur;
+	unsigned long long avg_dur;
+} ExitInfo;
+
+// 链表节点
+typedef struct Node {
+    ExitInfo data;
+    struct Node* next;
+} Node;
+
+Node* exitInfoBuffer = NULL;
+
+void addExitInfo(Node** head, int exit_reason, const char* info,unsigned long long dur,int count) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data.exit_reason = exit_reason;
+    strncpy(newNode->data.info, info, sizeof(newNode->data.info));
+    newNode->next = NULL;
+	newNode->data.total_dur = dur;
+	newNode->data.avg_dur = dur / count;
+
+    // 检查是否已经存在相同 exit reason 的信息
+    Node* current = *head;
+    Node* previous = NULL;
+    while (current != NULL) {
+        if (current->data.exit_reason == exit_reason) {
+            // 更新已存在的信息
+            strncpy(current->data.info, info, sizeof(current->data.info));
+			current->data.total_dur=dur+current->data.total_dur;
+			current->data.avg_dur=current->data.total_dur/count;
+            free(newNode); // 释放新节点，因为信息已经更新
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+    // 没有找到相同的 exit reason，将新节点添加到链表
+    if (previous != NULL) {
+        previous->next = newNode;
+    } else {
+        *head = newNode;
+    }
+}
+
+// 查找指定退出原因的信息
+const char* findExitInfo(Node* head, int exit_reason) {
+    Node* current = head;
+    while (current != NULL) {
+        if (current->data.exit_reason == exit_reason) {
+            return current->data.info;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+// 释放链表
+void freeExitInfoList(Node* head) {
+    while (head != NULL) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void printExitInfo(Node* head) {
+    Node* current = head;
+	printf("%-23s %-10s %-14s %-8s %-13s \n", "EXIT_REASON", "COMM","PID/TID","COUNT","AVG_DURATION(ns)"/*,"PCT"*/);
+    while (current != NULL) {
+		printf("%-2d/%-20s %-32s %-13llu \n", current->data.exit_reason,getExitReasonName(current->data.exit_reason), current->data.info,current->data.avg_dur);
+        current = current->next;
+    }
+}
+
+int doesVmProcessExist(pid_t pid) {
+    char proc_name[256];
+    snprintf(proc_name, sizeof(proc_name), "/proc/%d/cmdline", pid);
+    FILE *file = fopen(proc_name, "r");
+    if (file) {
+        size_t size;
+        size = fread(proc_name, 1, sizeof(proc_name), file);
+        if (size > 0) {
+            if (proc_name[size - 1] == '\n') {
+                proc_name[size - 1] = '\0';  // Remove newline character
+            }
+            if (strstr(proc_name, "qemu-system-x86_64") != NULL) {
+                fclose(file);
+                return 1;  // VmProcess name contains the target string
+            } else {
+                fclose(file);
+				fprintf(stderr, "Process exist!but is not vmprocess: %d\n", pid);
+                return 0;  // VmProcess name does not contain the target string
+            }
+        }
+        fclose(file);
+    }
+    fprintf(stderr, "Process name does not find: %d\n", pid);
+    return 0;  // VmProcess with the given PID not found
+}
+
+static struct env {
+	int monitoring_time;
+	pid_t vm_pid;
+} env={
+	.monitoring_time=0,
+	.vm_pid=0,
+};
+
+const char *argp_program_version = "kvm_exits 1.0";
+const char *argp_program_bug_address = "<nanshuaibo811@163.com>";
+const char argp_program_doc[] = "BPF program used for outputting VM exit reason\n";
+
+static const struct argp_option opts[] = {
+	{ "monitoring_time", 't', "TIME-SEC", 0, "Set the time for profiling VM exit event reasons" },
+	{ "vm_pid", 'p', "PID", 0, "Specify the virtual machine pid to monitor." },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case 't':
+		errno = 0;
+		env.monitoring_time = strtol(arg, NULL, 10);
+		if (errno || env.monitoring_time <= 0) {
+			fprintf(stderr, "Invalid duration: %s\n", arg);
+			argp_usage(state);
+		}
+        else{
+            alarm(env.monitoring_time);
+        }
+		break;
+	case 'p':
+		env.vm_pid=strtol(arg, NULL, 10);
+		if(env.vm_pid<=0 || doesVmProcessExist(env.vm_pid)==0 ){
+			fprintf(stderr, "Invalid vm_pid: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case ARGP_KEY_ARG:
+		argp_usage(state);
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static const struct argp argp = {
+	.options = opts,
+	.parser = parse_arg,
+	.doc = argp_program_doc,
+};
+
+
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
 	return vfprintf(stderr, format, args);
@@ -112,13 +273,19 @@ static volatile bool exiting = false;
 static void sig_handler(int sig)
 {
 	exiting = true;
+	printf("\n");
+	printf("---------------------------------------------------------------------------\n");
+	printExitInfo(exitInfoBuffer);
+	freeExitInfoList(exitInfoBuffer);
 }
 
 static int handle_event(void *ctx, void *data, size_t data_sz)
 {
 	const struct event *e = data;
-	printf("%-2d/%-20s %-10s %-5u/%-8u %-10d %-12llu %.2f%%\n", e->reason_number,getExitReasonName(e->reason_number), e->comm, e->pid,e->tid,e->count,e->duration_ns,(double)e->count / e->total * 100.0);
-
+	char info_buffer[256];
+	printf("%-2d/%-20s %-10s %-5u/%-8u %-8d %-13llu \n", e->reason_number,getExitReasonName(e->reason_number), e->comm, e->pid,e->tid,e->count,e->duration_ns/*,(double)e->count / e->total * 100.0*/);
+	snprintf(info_buffer, sizeof(info_buffer), "%-10s %-5u/%-8u %-8d", e->comm, e->pid,e->tid,e->count);
+	addExitInfo(&exitInfoBuffer,e->reason_number,info_buffer,e->duration_ns,e->count);
 	return 0;
 }
 
@@ -128,7 +295,10 @@ int main(int argc, char **argv)
 	struct kvm_exits_bpf *skel;
 	int err;
 
-    alarm(3);
+    /* Parse command line arguments */
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
 
 	/* Set up libbpf errors and debug info callback */
 	libbpf_set_print(libbpf_print_fn);
@@ -143,7 +313,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Failed to open BPF skeleton\n");
 		return 1;
 	}
-
+	skel->rodata->vm_pid = env.vm_pid;
 	/* Load & verify BPF programs */
 	err = kvm_exits_bpf__load(skel);
 	if (err) {
@@ -166,7 +336,7 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 		/* Process events */
-	printf("%-23s %-10s %-14s %-10s %-11s  %-10s \n", "EXIT_REASON", "COMM","PID/TID","COUNT","DURATION(ns)","PCT");
+	printf("%-23s %-10s %-14s %-8s %-13s \n", "EXIT_REASON", "COMM","PID/TID","COUNT","DURATION(ns)"/*,"PCT"*/);
 	while (!exiting) {
 		err = ring_buffer__poll(rb, 10 /* timeout, ms */);
 		/* Ctrl-C will cause -EINTR */
@@ -180,6 +350,7 @@ int main(int argc, char **argv)
 		}
 	}
 cleanup:
+    /* Clean up */
 	ring_buffer__free(rb);
 	kvm_exits_bpf__destroy(skel);
 	return -err;
