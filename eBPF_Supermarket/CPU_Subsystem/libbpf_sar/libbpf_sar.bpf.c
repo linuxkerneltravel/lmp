e8e55d5c95c8d1b88055833a3c9251e75fece6fb
Zhangxinyi <643470801@qq.com>
2023-11-16 19:06:08 +0800

    add sotfirq

--- eBPF_Supermarket/CPU_Subsystem/libbpf_sar/libbpf_sar.bpf.c
+++ eBPF_Supermarket/CPU_Subsystem/libbpf_sar/libbpf_sar.bpf.c
@@ -25,6 +25,10 @@
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 const volatile long long unsigned int forks_addr = 0;
+struct __softirq_info {
+	u64 pad;
+	u32 vec;
+};
 
 // 计数表格，第0项为上下文切换次数，第1项为总共新建进程个数
 struct {
@@ -33,9 +37,23 @@ struct {
 	__type(key, int);
 	__type(value, u64);
 } countMap SEC(".maps");
+//记录软中断开始时间
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __uint(max_entries, 4096);
+    __type(key, u32);
+    __type(value, u64);
+} softirqCpuEnterTime SEC(".maps");
+//软中断结束时间
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} softirqLastTime SEC(".maps");
 
 // 统计fork数
-SEC("kprobe/finish_task_switch.isra.0")
+SEC("kprobe/finish_task_switch")
 int kprobe__finish_task_switch(struct pt_regs *ctx)
 {
     u32 key = 0;
@@ -51,4 +69,28 @@ int kprobe__finish_task_switch(struct pt_regs *ctx)
     }
 
     return 0;
+}
+
+//统计软中断执行时间
+SEC("tracepoint/irq/softirq_entry")
+int trace_softirq_entry(struct __softirq_info *info) {
+	u32 key = info->vec;
+	u64 val = bpf_ktime_get_ns();
+	bpf_map_update_elem(&softirqCpuEnterTime, &key, &val, BPF_ANY);
+	return 0;
+}
+
+SEC("tracepoint/irq/softirq_exit")
+int trace_softirq_exit(struct __softirq_info *info) {
+	u32 key = info->vec;
+	u64 now = bpf_ktime_get_ns(), *valp = 0;
+	valp =bpf_map_lookup_elem(&softirqCpuEnterTime, &key);
+	if (valp) {
+		u64 last_time = now - *valp;
+		u32 key0 = 0;
+		valp = bpf_map_lookup_elem(&softirqLastTime, &key0);
+		if (!valp) bpf_map_update_elem(&softirqLastTime, &key0, &last_time, BPF_ANY);
+		else *valp += last_time;	
+	return 0;
+    }
 }
