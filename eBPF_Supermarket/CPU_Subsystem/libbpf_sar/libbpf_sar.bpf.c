c20b7e8a3417f51df811ffa55dcca53ff750dab7
Zhangxinyi <643470801@qq.com>
2023-11-16 21:04:29 +0800

    add softirq

--- eBPF_Supermarket/CPU_Subsystem/libbpf_sar/libbpf_sar.bpf.c
+++ eBPF_Supermarket/CPU_Subsystem/libbpf_sar/libbpf_sar.bpf.c
@@ -26,7 +26,7 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 const volatile long long unsigned int forks_addr = 0;
 
-// 计数表格，第0项为所统计fork数，第1项为进程切换数,第2项为运行队列长度
+// 计数表格，第0项为所统计fork数，第1项为进程切换数
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);//基于数组的映射
 	__uint(max_entries, 3);//countMap 可以存储最多 3 对键值对
@@ -37,16 +37,11 @@ struct {
 // 记录开始的时间
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__uint(max_entries, 1);
+	__uint(max_entries, 4096);
 	__type(key, int);
 	__type(value, u64);
 } start SEC(".maps");//记录时间戳；
 
-//环形缓冲区；
-struct {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 256 * 1024);
-} rb SEC(".maps");
 
 //cswch_args结构体
 struct cswch_args {
@@ -79,7 +74,7 @@ __type(value, int);
 
 
 // 统计fork数
-SEC("kprobe/finish_task_switch.isra.0")
+SEC("kprobe/finish_task_switch")
 int kprobe__finish_task_switch(struct pt_regs *ctx)
 {
     u32 key = 0;
@@ -96,8 +91,24 @@ int kprobe__finish_task_switch(struct pt_regs *ctx)
 
     return 0;
 }
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __uint(max_entries, 4096);
+    __type(key, u32);
+    __type(value, u64);
+} softirqCpuEnterTime SEC(".maps");//记录软中断开始时间
 
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} softirqLastTime SEC(".maps");//软中断结束时间
 
+struct __softirq_info {
+	u64 pad;
+	u32 vec;
+};
 
 //获取进程切换数;
 SEC("tracepoint/sched/sched_switch")//静态挂载点
@@ -114,18 +125,8 @@ int trace_sched_switch2(struct cswch_args *info) {
 		// 将下一个进程的进程号赋给pid
 		pid_t pid = next;
 		u64 time = bpf_ktime_get_ns()/1000;//获取当前时间，ms；
-
 		// Step1: 记录next进程的起始时间
 		bpf_map_update_elem(&start,&pid,&time,BPF_ANY);//上传当前时间到start map中
-		//procStartTime.update(&pid, &time);//python
-
-		// Step2: Syscall时间处理
-		// record_sysc(time, prev, next);
-
-		// Step3: UserMode时间处理
-		// record_user(time, prev, next);
-
-		// Step4: 记录上下文切换的总次数
 		valp =  bpf_map_lookup_elem(&countMap,&key);
 		if (!valp) {
 			// 没有找到表项
@@ -184,18 +185,40 @@ int trace_sched_switch(struct cswch_args *info) {
 //统计运行队列长度
 SEC("kprobe/update_rq_clock")
 int kprobe_update_rq_clock(struct pt_regs *ctx){
-    u32 key = 2;
-    u32 rqkey = 0;
-    struct rq *p_rq = 0;
+    u32 key     = 0;
+	u32 rqkey	= 0;
+	struct rq *p_rq = 0;
     p_rq = (struct rq *)bpf_map_lookup_elem(&rq_map, &rqkey);
-    if (!p_rq) {
-        return 0;
-    }
-
+	if (!p_rq) {
+		return 0;
+	}
     bpf_probe_read_kernel(p_rq, sizeof(struct rq), (void *)PT_REGS_PARM1(ctx));
-    //使用bpf_probe_read_kernel函数将内核空间中的数据复制到p_rq所指向的内存区域中，以便后续对该数据进行访问和操作。
-    u64 val = p_rq->nr_running;
-    bpf_map_update_elem(&countMap,&key,&val,BPF_ANY);
-    return 0;
+	//使用bpf_probe_read_kernel函数将内核空间中的数据复制到p_rq所指向的内存区域中，以便后续对该数据进行访问和操作。
+	u64 val = p_rq->nr_running;
+	bpf_map_update_elem(&runqlen,&key,&val,BPF_ANY);
+	return 0;
 }
 
+//软中断
+SEC("tracepoint/irq/softirq_entry")
+int trace_softirq_entry(struct __softirq_info *info) {
+	u32 key = info->vec;
+	u64 val = bpf_ktime_get_ns();
+	bpf_map_update_elem(&softirqCpuEnterTime, &key, &val, BPF_ANY);
+	return 0;
+}
+
+SEC("tracepoint/irq/softirq_exit")
+int trace_softirq_exit(struct __softirq_info *info) {
+	u32 key = info->vec;
+	u64 now = bpf_ktime_get_ns(), *valp = 0;
+	valp =bpf_map_lookup_elem(&softirqCpuEnterTime, &key);
+	if (valp) {
+		u64 last_time = now - *valp;
+		u32 key0 = 0;
+		valp = bpf_map_lookup_elem(&softirqLastTime, &key0);
+		if (!valp) bpf_map_update_elem(&softirqLastTime, &key0, &last_time, BPF_ANY);
+		else *valp += last_time;
+	return 0;
+}
+}
