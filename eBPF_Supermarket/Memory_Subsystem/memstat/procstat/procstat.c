243aaa45b3b666d3aa2ab0b4c44a661699716445
syxl-time <953879556@qq.com>
2023-11-18 23:01:10 +0800

    添加工具运行环境

--- eBPF_Supermarket/Memory_Subsystem/memstat/procstat/procstat.c
+++ eBPF_Supermarket/Memory_Subsystem/memstat/procstat/procstat.c
@@ -25,24 +25,25 @@ static const struct argp_option opts[] = {
 };
 
 // 命令行参数解析函数
-static error_t parse_arg(int key, char *arg, struct argp_state *state) {
-    switch (key) {
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+        switch (key) {
         case 'p':
             env.choose_pid = strtol(arg, NULL, 10);
             break;
         case 't':
-            env.time_s = strtol(arg, NULL, 10);
+			env.time_s = strtol(arg, NULL, 10);
             break;
-        case 'r':
-            env.rss = true;
+		case 'r':
+			env.rss = true;
             break;
         case ARGP_KEY_ARG:
             argp_usage(state);
             break;
         default:
             return ARGP_ERR_UNKNOWN;
-    }
-    return 0;
+        }
+        return 0;
 }
 
 // 命令行解析器
@@ -56,10 +57,12 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va
     return vfprintf(stderr, format, args);
 }
 
-// 信号处理函数
-static volatile bool exiting;
-static void sig_handler(int sig) {
-    exiting = true;
+// 信号处理函数，处理Ctrl-C
+static volatile bool exiting = false;
+
+static void sig_handler(int sig)
+{
+	exiting = true;
 }
 
 // 毫秒级睡眠函数
@@ -97,12 +100,14 @@ static int handle_event(void *ctx, void *data, size_t data_sz) {
         }
     }
 
-    if(env.time_s) {
-        msleep(env.time_s);
-    } else {
-        msleep(1000);
-    }
-    return 0;
+    // 根据延时时间休眠
+	if(env.time_s != NULL) {
+		msleep(env.time_s);
+	}
+	else {
+		msleep(0);
+	}
+	return 0;
 }
 
 int main(int argc, char **argv) {
@@ -112,19 +117,21 @@ int main(int argc, char **argv) {
 
     // 解析命令行参数
     err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
-    if (err) {
+    if (err)
         return err;
-    }
 
     // 设置libbpf的严格模式和调试信息回调
+    /* Set up libbpf errors and debug info callback */
     libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
     libbpf_set_print(libbpf_print_fn);
 
     // 注册信号处理函数
+    /* Cleaner handling of Ctrl-C */
     signal(SIGINT, sig_handler);
-    signal(SIGTERM, sig_handler);
+	signal(SIGTERM, sig_handler);
 
     // 打开BPF程序并加载验证BPF程序
+    /* Load and verify BPF application */
     skel = procstat_bpf__open();
     if (!skel) {
         fprintf(stderr, "Failed to open and load BPF skeleton\n");
@@ -132,6 +139,7 @@ int main(int argc, char **argv) {
     }
 
     // 加载并验证BPF程序
+    /* Load & verify BPF programs */
     err = procstat_bpf__load(skel);
     if (err) {
         fprintf(stderr, "Failed to load and verify BPF skeleton\n");
@@ -139,6 +147,7 @@ int main(int argc, char **argv) {
     }
 
     // 注册BPF程序的tracepoints
+    /* Attach tracepoints */
     err = procstat_bpf__attach(skel);
     if (err) {
         fprintf(stderr, "Failed to attach BPF skeleton\n");
@@ -146,6 +155,7 @@ int main(int argc, char **argv) {
     }
 
     // 创建用于处理事件的环形缓冲区
+    /* Set up ring buffer polling */
     rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
     if (!rb) {
         err = -1;
@@ -154,28 +164,30 @@ int main(int argc, char **argv) {
     }
 
     // 打印表头
+    /* Process events */
     if(env.rss) {
         printf("%-8s %-8s %-8s %-8s %-8s %-8s %-8s\n", "TIME", "PID", "VMSIZE", "VMDATA", "VMSTK", "VMPTE", "VMSWAP");
     } else {        
         printf("%-8s %-8s %-8s %-8s %-8s %-8s\n", "TIME", "PID", "SIZE", "RSSANON", "RSSFILE", "RSSSHMEM");
     }
 
-    // 处理事件，根据命令行参数选择要显示的信息
-    while (!exiting) {
-        err = ring_buffer__poll(rb, 100 /* 超时时间，毫秒 */);
-        // Ctrl-C会导致返回-EINTR
-        if (err == -EINTR) {
-            err = 0;
-            break;
-        }
-        if (err < 0) {
-            printf("Error polling perf buffer: %d\n", err);
-            break;
-        }
-    }
+    // 处理事件
+	while (!exiting) {
+		err = ring_buffer__poll(rb, 100 /* timeout, ms */);
+		/* Ctrl-C will cause -EINTR */
+		if (err == -EINTR) {
+			err = 0;
+			break;
+		}
+		if (err < 0) {
+			printf("Error polling perf buffer: %d\n", err);
+			break;
+		}
+	}
 
 cleanup:
     // 清理并释放资源
+    /* Clean up */
     ring_buffer__free(rb);
     procstat_bpf__destroy(skel);
 
