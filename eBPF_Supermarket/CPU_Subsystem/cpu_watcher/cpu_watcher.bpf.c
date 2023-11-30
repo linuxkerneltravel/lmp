9472da94e314073c004008a36fe25ab3d28e73a1
albertxu216 <1418106416@qq.com>
2023-11-30 13:15:41 +0800

    Cs_dely & Libbpf_sar功能合进Cpuwatcher , Add Idle & Thread

---
+++ eBPF_Supermarket/CPU_Subsystem/cpu_watcher/cpu_watcher.bpf.c
@@ -0,0 +1,422 @@
+// Copyright 2023 The LMP Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// author: zhangziheng0525@163.com
+//
+// kernel-mode code for libbpf sar
+
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "cpu_watcher.h"
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+const volatile long long unsigned int forks_addr = 0;
+
+#define PF_IDLE			0x00000002	/* I am an IDLE thread */
+#define PF_KTHREAD		0x00200000	/* I am a kernel thread */
+
+/*----------------------------------------------*/
+/*          cs_delay相关maps                    */
+/*----------------------------------------------*/
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, u64);
+} start SEC(".maps");//记录时间戳；
+
+/*----------------------------------------------*/
+/*         sar相关maps                          */
+/*----------------------------------------------*/
+// 计数表格，第0项为所统计fork数，第1项为进程切换数,
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);//基于数组的映射
+	__uint(max_entries, 3);//countMap 可以存储最多 3 对键值对
+	__type(key, int);
+	__type(value, u64);
+} countMap SEC(".maps");
+
+// 记录开始的时间
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 4096);
+	__type(key, pid_t);
+	__type(value, u64);
+} procStartTime SEC(".maps");//记录时间戳；
+
+//环形缓冲区；
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 256 * 1024);
+} rb SEC(".maps");
+
+
+// 储存运行队列rq的全局变量
+struct {
+__uint(type, BPF_MAP_TYPE_ARRAY);
+__uint(max_entries, 1);
+__type(key, u32);
+__type(value, struct rq);
+} rq_map SEC(".maps");
+
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, u32);
+	__type(value, int);
+} runqlen SEC(".maps");//多CPU数组
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __uint(max_entries, 4096);
+    __type(key, u32);
+    __type(value, u64);
+} softirqCpuEnterTime SEC(".maps");//记录软中断开始时间
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} softirqLastTime SEC(".maps");//软中断结束时间
+
+// 记录开始的时间
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+	__uint(max_entries, 8192);//该哈希表可容纳8192个条目
+	__type(key, u32);
+	__type(value, u64);
+} irq_cpu_enter_start SEC(".maps");//irq_start是一个哈希类型的 eBPF map，用于存储开始执行时的时间戳。
+
+//记录上次中断时间
+struct {
+__uint(type, BPF_MAP_TYPE_ARRAY);
+__uint(max_entries, 1);
+__type(key, u32);
+__type(value, u64);
+} irq_Last_time SEC(".maps");
+
+// 储存cpu进入空闲的起始时间
+struct {
+__uint(type, BPF_MAP_TYPE_ARRAY);
+__uint(max_entries, 128);
+__type(key, u32);
+__type(value, u64);
+} idleStart SEC(".maps");
+
+// 储存cpu进入空闲的持续时间
+struct {
+__uint(type, BPF_MAP_TYPE_ARRAY);
+__uint(max_entries, 1);
+__type(key, u32);
+__type(value, u64);
+} idleLastTime SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u64);
+} kt_LastTime SEC(".maps");
+
+/*----------------------------------------------*/
+/*          cs_delay跟踪函数                    */
+/*----------------------------------------------*/
+
+SEC("kprobe/schedule")
+int BPF_KPROBE(schedule)
+{
+	u64 t1;
+	t1 = bpf_ktime_get_ns()/1000;	//bpf_ktime_get_ns返回自系统启动以来所经过的时间(以纳秒为单位)。不包括系统挂起的时间。
+	int key =0;
+	bpf_map_update_elem(&start,&key,&t1,BPF_ANY);
+	return 0;
+}
+
+SEC("kretprobe/schedule")
+int BPF_KRETPROBE(schedule_exit)
+{	
+	u64 t2 = bpf_ktime_get_ns()/1000;
+	u64 t1,delay;
+	int key = 0;
+	u64 *val = bpf_map_lookup_elem(&start,&key);
+	if (val != 0) 
+	{
+        	t1 = *val;
+        	delay = t2 - t1;
+			bpf_map_delete_elem(&start, &key);
+	}else{
+		return 0;
+	}
+	
+	struct event *e;
+	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+	if (!e)	return 0;	
+	
+	e->t1=t1;//开始时间
+	e->t2=t2;//结束时间
+	e->delay=delay;//时间间隔
+	
+	/* 成功地将其提交到用户空间进行后期处理 */
+	bpf_ringbuf_submit(e, 0);
+	
+	return 0;
+}
+
+/*----------------------------------------------*/
+/*           sar跟踪函数                         */
+/*----------------------------------------------*/
+// 统计fork数
+SEC("kprobe/finish_task_switch")
+int kprobe__finish_task_switch(struct pt_regs *ctx)
+{
+    u32 key = 0;
+    u64 val, *valp = NULL;
+    unsigned long total_forks;
+     
+    if(forks_addr !=0){
+        valp = (u64 *)forks_addr;
+        bpf_probe_read_kernel(&total_forks, sizeof(unsigned long), valp);
+        key = 1;
+        val = total_forks;
+        bpf_map_update_elem(&countMap,&key,&val,BPF_ANY);
+    }
+    return 0;
+}
+
+//获取进程切换数;
+SEC("tracepoint/sched/sched_switch")//静态挂载点
+int trace_sched_switch2(struct cswch_args *info) {
+	//从参数info中获取上一个(prev)和下一个(next)进程的进程号
+	pid_t prev = info->prev_pid, next = info->next_pid;//定义上一个、下一个进程的进程号
+	
+	// 只有当上一个进程和下一个进程不相同时才执行以下操作，相同则代表是同一个进程
+	if (prev != next) {
+		u32 key = 0;
+		u64 *valp, delta, cur;
+		struct task_struct *ts;
+
+		// 将下一个进程的进程号赋给pid
+		pid_t pid = next;
+		u64 time = bpf_ktime_get_ns()/1000;//获取当前时间，ms；
+
+		// Step1: 记录next进程的起始时间
+		bpf_map_update_elem(&procStartTime,&pid,&time,BPF_ANY);//上传当前时间到start map中
+		//procStartTime.update(&pid, &time);//python
+
+		// Step2: Syscall时间处理
+		// record_sysc(time, prev, next);
+
+		// Step3: UserMode时间处理
+		// record_user(time, prev, next);
+
+		// Step4: 记录上下文切换的总次数
+		valp =  bpf_map_lookup_elem(&countMap,&key);
+		if (!valp) {
+			// 没有找到表项
+			u64 initval = 1;
+			bpf_map_update_elem(&countMap,&key,&initval,BPF_ANY);//初始化切换次数到countMap中
+		}
+		else *valp += 1;
+		//bpf_map_update_elem(&countMap,&key,&valp,BPF_ANY);//上传当前切换次数到countMap中
+	}
+
+	return 0;
+}
+
+SEC("kprobe/finish_task_switch") 
+int BPF_KPROBE(finish_task_switch,struct task_struct *prev){
+	pid_t pid=BPF_CORE_READ(prev,pid);
+	u64 *val, time = bpf_ktime_get_ns();
+	u64 delta;
+	// Step1: 记录内核进程（非IDLE）运行时间
+	if ((BPF_CORE_READ(prev,flags) & PF_KTHREAD) && pid!= 0) {
+		val = bpf_map_lookup_elem(&procStartTime, &pid);
+		if (val) {
+			u32 key = 0;
+			delta = time - *val*1000;
+			val = bpf_map_lookup_elem(&kt_LastTime, &key);
+			if (val) *val += delta;
+			else bpf_map_update_elem(&kt_LastTime, &key, &delta, BPF_ANY);
+		}
+	} 
+	return 0;
+}
+
+/*
+SEC("kprobe/finish_task_switch")//动态挂载点
+int trace_sched_switch(struct cswch_args *info) {
+	//从参数info中获取上一个(prev)和下一个(next)进程的进程号
+	pid_t prev = info->prev_pid, next = info->next_pid;//定义上一个、下一个进程的进程号
+	
+	// 只有当上一个进程和下一个进程不相同时才执行以下操作，相同则代表是同一个进程
+	if (prev != next) {
+		u32 key = 2;
+		u64 *valp, delta, cur;
+		struct task_struct *ts;
+
+		// 将下一个进程的进程号赋给pid
+		pid_t pid = next;
+		u64 time = bpf_ktime_get_ns()/1000;//获取当前时间，ms；
+
+		// Step1: 记录next进程的起始时间
+		bpf_map_update_elem(&procStartTime,&pid,&time,BPF_ANY);//上传当前时间到start map中
+		//procStartTime.update(&pid, &time);//python
+
+		// Step2: Syscall时间处理
+		// record_sysc(time, prev, next);
+
+		// Step3: UserMode时间处理
+		// record_user(time, prev, next);
+
+		// Step4: 记录上下文切换的总次数
+		valp =  bpf_map_lookup_elem(&countMap,&key);
+		if (!valp) {
+			// 没有找到表项
+			u64 initval = 1;
+			bpf_map_update_elem(&countMap,&key,&initval,BPF_ANY);//初始化切换次数到countMap中
+		}
+		else *valp += 1;
+		//bpf_map_update_elem(&countMap,&key,&valp,BPF_ANY);//上传当前切换次数到countMap中
+	}
+
+	return 0;
+}
+*/
+
+
+//统计运行队列长度
+SEC("kprobe/update_rq_clock")
+int kprobe_update_rq_clock(struct pt_regs *ctx){
+    u32 key = 0;
+    u32 rqkey = 0;
+    struct rq *p_rq = 0;
+    p_rq = (struct rq *)bpf_map_lookup_elem(&rq_map, &rqkey);
+    if (!p_rq) {
+        return 0;
+    }
+
+    bpf_probe_read_kernel(p_rq, sizeof(struct rq), (void *)PT_REGS_PARM1(ctx));
+    //使用bpf_probe_read_kernel函数将内核空间中的数据复制到p_rq所指向的内存区域中，以便后续对该数据进行访问和操作。
+    u64 val = p_rq->nr_running;
+    bpf_map_update_elem(&runqlen,&key,&val,BPF_ANY);
+    return 0;
+}
+
+//软中断
+SEC("tracepoint/irq/softirq_entry")
+int trace_softirq_entry(struct __softirq_info *info) {
+
+	u32 key = info->vec;//获取软中断向量号，作为 BPF Map 的键
+	u64 val = bpf_ktime_get_ns();//获取当前时间
+	bpf_map_update_elem(&softirqCpuEnterTime, &key, &val, BPF_ANY);
+	return 0;
+}
+
+SEC("tracepoint/irq/softirq_exit")
+int trace_softirq_exit(struct __softirq_info *info) {
+
+	u32 key = info->vec;
+	u64 now = bpf_ktime_get_ns(), *valp = 0;
+
+	valp =bpf_map_lookup_elem(&softirqCpuEnterTime, &key);
+	if (valp) {
+		// 找到表项
+		u64 last_time = now - *valp;
+		u32 key0 = 0;
+
+		valp = bpf_map_lookup_elem(&softirqLastTime, &key0);//在lasttime数组中找，因为大小是1
+		if (!valp) bpf_map_update_elem(&softirqLastTime, &key0, &last_time, BPF_ANY);
+		else *valp += last_time;
+	/*当处理软中断退出事件时，代码使用了bpf_map_lookup_elem函数查询softirqLastTime BPF Map中与键key0对应的条目，
+	其中key0为常量0，因为softirqLastTime是一个大小为1的数组。如果该条目不存在，则说明这是第一次处理软中断退出事件，
+	需要将last_time的值插入到softirqLastTime表中；否则，说明已经统计过该软中断的运行时间，需要将本次运行时间累加到原有的运行时间上。*/
+	}	
+	return 0;
+}
+
+/*irqtime：CPU响应irq中断所占用的时间。
+注意这是所有CPU时间的叠加，平均到每个CPU应该除以CPU个数。*/
+SEC("tracepoint/irq/irq_handler_entry")
+int trace_irq_handler_entry(struct __irq_info *info) {
+	
+	u32 key = info->irq;//获取irq硬中断的信息
+	u64 ts = bpf_ktime_get_ns();//获取当前时间
+
+    bpf_map_update_elem(&irq_cpu_enter_start, &key, &ts, BPF_ANY);//将时间戳上传;
+	return 0;
+}
+
+SEC("tracepoint/irq/irq_handler_exit")
+int trace_irq_handler_exit(struct __irq_info *info) {
+
+	u32 key = info->irq;
+	u64 now = bpf_ktime_get_ns(), *ts = 0;
+    // struct event *e;
+    // e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+	// if (!e)
+	// 	return 0;
+
+    ts = bpf_map_lookup_elem(&irq_cpu_enter_start, &key);//ts中断开始的时间;
+    // if(ts==NULL){
+    //     return 0;
+    // }
+	if (ts) {
+		// 找到表项
+        u64 last_time = now - *ts;//记录持续时间
+	    u32 key0 = 0;
+		ts = bpf_map_lookup_elem(&irq_Last_time, &key0);//在irq_Last_time数组中找是否有该中断对应的中断持续时间;
+
+		if (!ts)    bpf_map_update_elem(&irq_Last_time, &key0, &last_time, BPF_ANY);//在irq_Last_time数组中记录该中断对应的中断持续时间;
+		else        *ts += last_time;//该中断已记录持续时间，将ts所指向的持续时间+这次的持续时间；
+		
+	}
+
+    // e->irqtime = bpf_map_lookup_elem(&irq_Last_time, &key0);
+    // bpf_ringbuf_submit(e, 0);//将填充的event提交到BPF缓冲区,以供用户空间进行后续处理
+	return 0;
+}
+
+
+//tracepoint:power_cpu_idle 表征了CPU进入IDLE的状态，比较准确
+SEC("tracepoint/power/cpu_idle")
+int trace_cpu_idle(struct idleStruct *pIDLE) {
+	u64 delta, time = bpf_ktime_get_ns();
+	u32 key = pIDLE->cpu_id;
+	// 按cpuid记录空闲的开始，这十分重要，因为IDLE-0进程可同时运行在两个核上
+
+	if (pIDLE->state == -1) {
+		// 结束idle
+		u64 *valp = bpf_map_lookup_elem(&idleStart,&key);
+		if (valp && *valp != 0) {
+            /*找到初始idle时间*/
+			delta = time - *valp;//持续空闲时间=当前时间-进入空闲时间；
+			key = 0;
+			valp = bpf_map_lookup_elem(&idleLastTime,&key);
+			if (valp) *valp += delta;//找到持续空闲时间,持续空闲时间更新;
+			else bpf_map_update_elem(&idleLastTime,&key,&delta,BPF_ANY);//初次记录持续空闲时间;
+		}
+	} else {
+		// 开始idle
+		u64 val = time;
+		bpf_map_update_elem(&idleStart,&key,&time,BPF_ANY);
+	}
+	return 0;
+}
+
+
---
+++ eBPF_Supermarket/CPU_Subsystem/cpu_watcher/cpu_watcher.c
@@ -0,0 +1,427 @@
+// Copyright 2023 The LMP Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// author: zhangziheng0525@163.com
+//
+// user-mode code for libbpf sar
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <signal.h>
+#include <argp.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include "cpu_watcher.skel.h"
+#include "cpu_watcher.h"
+
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+typedef long long unsigned int u64;
+typedef unsigned int u32;
+static volatile bool exiting = false;//全局变量，表示程序是否正在退出
+
+struct cpu_watcher_bpf *skel;//用于自行加载和运行BPF程序的结构体，由libbpf自动生成并提供与之关联的各种功能接口；
+u64 softirq = 0;//初始化softirq;
+u64 irqtime = 0;//初始化irq;
+u64 idle = 0;//初始化idle;
+u64 sched = 0;
+u64 proc = 0;
+unsigned long ktLastTime = 0;
+
+// sar 工具的参数设置
+static struct env {
+	int time;
+	bool enable_proc;
+	bool libbpf_sar;
+	bool cs_delay;
+
+} env = {
+	.time = 0,
+	.enable_proc = false,
+	.libbpf_sar = false,
+	.cs_delay = false,
+};
+
+/*设置传参*/
+const char argp_program_doc[] ="cpu wacher is in use ....\n";
+static const struct argp_option opts[] = {
+	{ "time", 't', "TIME-SEC", 0, "Max Running Time(0 for infinite)" },
+	{"libbpf_sar", 's',	0,0,"print sar_info (the data of cpu)"},
+	{"cs_delay", 'c',	0,0,"print cs_delay (the data of cpu)"},
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "show the full help" },
+	{0},
+};
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+		case 't':
+			env.time = strtol(arg, NULL, 10);
+			if(env.time) alarm(env.time);
+                	break;
+		case 's':
+			env.libbpf_sar = true;
+			break;
+		case 'c':
+			env.cs_delay = true;
+			break;			
+		case 'h':
+			argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+static const struct argp argp = {
+    .options = opts,
+    .parser = parse_arg,
+    .doc = argp_program_doc,
+};
+
+
+static void sig_handler(int sig)//信号处理函数
+{
+	exiting = true;
+}//正在退出程序；
+
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+/*-----------------------------------------------------------------------------------------------------*/
+/*                         cs_delay处理函数                                                             */
+/*-----------------------------------------------------------------------------------------------------*/
+int count[25]={0};//定义一个count数组，用于汇总schedul()调度时间，以log2(时间间隔)为统计依据；
+static int handle_event(void *ctx, void *data,unsigned long data_sz)
+{
+	const struct event *e = data;
+	printf("t1:%lu  t2:%lu  delay:%lu\n",e->t1,e->t2,e->delay);
+
+	int dly=(int)(e->delay),i=0;
+	while (dly > 1){
+		dly /= 2;
+		i ++;
+	}
+	count[i]++;//记录时间间隔次数;
+	return 0;
+}
+static int print_hstgram(int i,int max,int per_len)
+{
+	int cnt=count[i];
+	if(per_len==1){
+		while(cnt>0){//打印
+			printf("*");
+			cnt--;
+		}
+	}
+	while(cnt-per_len>=0){//打印
+		printf("*");
+		cnt-=per_len;
+	}
+	printf("\n");
+	return per_len;
+}
+double pow(int n,int k)//实现pow函数
+{
+	if (k > 0)
+		return n * pow(n, k - 1);
+	else if (k == 0)
+		return 1;
+	else
+		return 1.0 / pow(n, -k);
+}
+static void histogram()
+{
+	int log10[15]={0},max=0,per_len=1;
+	for(int i=0;i<10;i++){//log10(count[i]);
+		int tmp=count[i],cnt=0;
+		while (tmp >= 10){
+			tmp /= 10;
+			cnt ++;//幂次
+		}
+		log10[cnt]++;
+	}
+
+	for(int i=0;i<10;i++){//找log10里的最大值；
+		if(max<log10[i])
+			max=i;
+	}
+
+	while(max>0){//pow(10,max);
+		per_len *=10 ;
+		max--;
+	}
+
+	time_t now = time(NULL);// 获取当前时间
+	struct tm *localTime = localtime(&now);// 将时间转换为本地时间结构
+	printf("\nTime : %02d:%02d:%02d \n",localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
+	printf("%-24s \t%-12s \t%-12s \n","cs_delay","Count","Distribution");
+	printf("%d\t=>\t%-8d \t%-12d \t|",0,1,count[0]);
+	print_hstgram(0,max,per_len);
+	printf("%d\t=>\t%-8d \t%-12d \t|",2,3,count[1]);
+	print_hstgram(1,max,per_len);
+	for(int i=2;i<20;i++){
+		printf("%d\t=>\t%-8d \t%-12d \t|",(int)pow(2,i),(int)pow(2,(i+1))-1,count[i]);
+		print_hstgram(i,max,per_len);
+	}
+	printf("per_len = %d\n",per_len);
+}
+
+
+/*-----------------------------------------------------------------------------------------------------*/
+/*                         libbpf_sar处理函数                                                           */
+/*-----------------------------------------------------------------------------------------------------*/
+// 根据符号名称从/proc/kallsyms文件中搜索对应符号地址
+u64 find_ksym(const char* target_symbol) {
+    FILE *file = fopen("/proc/kallsyms", "r");
+    if (file == NULL) {
+        perror("Failed to open /proc/kallsyms");
+        return 1;
+    }
+
+    char symbol_name[99];
+    u64 symbol_address = 0;
+
+    while (fscanf(file, "%llx %*c %s\n", &symbol_address, symbol_name) != EOF) {
+        if (strcmp(symbol_name, target_symbol) == 0) {
+            break;
+        }
+    }
+
+    fclose(file);
+
+    return symbol_address;
+}
+/*libbpf_sar处理函数*/
+static int print_all()
+{
+
+	/*proc:*/
+	int key_proc = 1;// 设置要查找的键值为0
+	int err_proc, fd_proc = bpf_map__fd(skel->maps.countMap);// 获取映射文件描述符
+	u64 total_forks;// 用于存储从映射中查找到的值
+	err_proc = bpf_map_lookup_elem(fd_proc, &key_proc, &total_forks); // 从映射中查找键为1的值
+	if (err_proc < 0) {//没找到
+		fprintf(stderr, "failed to lookup infos of total_forks: %d\n", err_proc);
+		return -1;
+	}
+	u64 __proc;
+	__proc = total_forks - proc;//计算差值;
+	proc = total_forks;
+
+	/*cswch:*/
+	int key_cswch = 0;// 设置要查找的键值为1
+	int err_cswch, fd_cswch = bpf_map__fd(skel->maps.countMap);// 获取映射文件描述符
+	u64 sched_total;// 用于存储从映射中查找到的值
+	err_cswch = bpf_map_lookup_elem(fd_cswch, &key_cswch, &sched_total); // 从映射中查找键为1的值
+	if (err_cswch < 0) {//没找到
+		fprintf(stderr, "failed to lookup infos of sched_total: %d\n", err_cswch);
+		return -1;
+	}
+	u64 __sched;
+	__sched = sched_total - sched;//计算差值;
+	sched = sched_total;
+
+	// /*runqlen:*/ 
+	// int key_runqlen = 0;// 设置要查找的键值为0
+	// int err_runqlen, fd_runqlen = bpf_map__fd(skel->maps.runqlen);// 获取映射文件描述符
+	// int runqlen;// 用于存储从映射中查找到的值
+	// err_runqlen = bpf_map_lookup_elem(fd_runqlen, &key_runqlen, &runqlen); // 从映射中查找键为1的值
+	// if (err_runqlen < 0) {//没找到
+	// 	fprintf(stderr, "failed to lookup infos of runqlen: %d\n", err_runqlen);
+	// 	return -1;
+	// }
+
+	/*irqtime:*/
+	int key_irqtime = 0;// 设置要查找的键值为0
+	int err_irqtime, fd_irqtime = bpf_map__fd(skel->maps.irq_Last_time);// 获取映射文件描述符
+	u64 __irqtime;// 用于存储从映射中查找到的值
+    __irqtime = irqtime;
+	err_irqtime = bpf_map_lookup_elem(fd_irqtime, &key_irqtime, &irqtime); // 从映射中查找键为1的值
+	if (err_irqtime < 0) {//没找到
+		fprintf(stderr, "failed to lookup infos of irqtime: %d\n", err_irqtime);
+		return -1;
+	}
+    u64 dtairqtime = (irqtime - __irqtime);
+
+	/*softirq:*/
+	int key_softirq = 0;// 设置要查找的键值为0
+	int err_softirq, fd_softirq = bpf_map__fd(skel->maps.softirqLastTime);// 获取映射文件描述符
+	u64 __softirq;// 用于存储从映射中查找到的值
+    __softirq = softirq;
+	err_softirq = bpf_map_lookup_elem(fd_softirq, &key_softirq, &softirq); // 从映射中查找键为1的值
+	if (err_softirq < 0) {//没找到
+		fprintf(stderr, "failed to lookup infos of softirq: %d\n", err_softirq);
+		return -1;
+	}
+    u64 dtasoftirq = (softirq - __softirq);
+
+	/*idle*/
+	int key_idle = 0;// 设置要查找的键值为0
+	int err_idle, fd_idle = bpf_map__fd(skel->maps.idleLastTime);// 获取映射文件描述符
+	u64 __idle;// 用于存储从映射中查找到的值
+    __idle = idle;
+	err_idle = bpf_map_lookup_elem(fd_idle, &key_idle, &idle); // 从映射中查找键为1的值
+	if (err_idle < 0) {//没找到
+		fprintf(stderr, "failed to lookup infos of idle: %d\n", err_idle);
+		return -1;
+	}
+    u64 dtaidle = (idle - __idle);	
+
+	/*kthread*/
+	int key_kthread = 0;
+	int err_kthread, fd_kthread = bpf_map__fd(skel->maps.kt_LastTime);
+	unsigned long  _ktLastTime=0; 
+	_ktLastTime = ktLastTime;
+	err_kthread = bpf_map_lookup_elem(fd_kthread, &key_kthread,&ktLastTime);
+	if (err_kthread < 0) {
+		fprintf(stderr, "failed to lookup infos: %d\n", err_kthread);
+		return -1;
+	}
+	unsigned long dtaKT = ktLastTime -_ktLastTime;
+
+
+
+
+	if(env.enable_proc){
+		//判断打印：
+		time_t now = time(NULL);// 获取当前时间
+		struct tm *localTime = localtime(&now);// 将时间转换为本地时间结构
+		printf("%02d:%02d:%02d %8llu %8llu %8llu %8llu  %8llu %8lu\n",
+				localTime->tm_hour, localTime->tm_min, localTime->tm_sec,__proc,__sched,dtairqtime/1000,dtasoftirq/1000,dtaidle/1000000,dtaKT/1000);
+	}
+	else{
+		env.enable_proc = true;
+	}
+
+    return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+	struct ring_buffer *rb = NULL;
+	int err;//用于存储错误码
+	const char* symbol_name = "total_forks";
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
+
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	/* 设置libbpf错误和调试信息回调 */
+	libbpf_set_print(libbpf_print_fn);	
+
+	/* 更干净地处理Ctrl-C
+	   SIGINT：由Interrupt Key产生，通常是CTRL+C或者DELETE。发送给所有ForeGround Group的进程
+       SIGTERM：请求中止进程，kill命令发送
+	*/
+	signal(SIGINT, sig_handler);		//注册一个信号处理函数 sig_handler，用于处理 Ctrl-C 信号（SIGINT）
+	signal(SIGTERM, sig_handler);
+
+	/* 打开BPF应用程序 */
+	skel = cpu_watcher_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+
+	skel->rodata->forks_addr = (u64)find_ksym(symbol_name);
+
+	/* 加载并验证BPF程序 */
+	err = cpu_watcher_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+	
+	/* 附加跟踪点处理程序 */
+	err = cpu_watcher_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+	
+	printf("Tracing for Data's... Ctrl-C to end\n");
+
+    // rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), print_all, NULL, NULL);
+	// if (!rb) {
+	// 	err = -1;
+	// 	fprintf(stderr, "Failed to create ring buffer\n");
+	// 	goto cleanup;
+	// }
+
+	if(env.libbpf_sar){
+		//printf("  time    proc/s  cswch/s  runqlen  irqTime/us  softirq/us  idle/ms  kthread/us  sysc/ms  utime/ms  sys/ms  BpfCnt\n");
+		//printf("  time   softirq\n");
+		printf("  time    proc/s  cswch/s  irqTime/us  softirq/us  idle/ms  kthread/us\n");
+	}
+	else if(env.cs_delay){
+		/* 设置环形缓冲区轮询 */
+		rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);	//ring_buffer__new() API，允许在不使用额外选项数据结构下指定回调
+		if (!rb) {
+			err = -1;
+			fprintf(stderr, "Failed to create ring buffer\n");
+			goto cleanup;
+		}
+	}
+	/* 处理事件 */
+	while (!exiting) {
+		sleep(1);
+		if(env.libbpf_sar){
+			err = print_all();
+			/* Ctrl-C will cause -EINTR */
+			if (err == -EINTR) {
+				err = 0;
+				break;
+			}
+			if (err < 0) {
+        	    printf("Error polling perf buffer: %d\n", err);
+				break;
+			}
+		}
+        else if(env.cs_delay){
+			err = ring_buffer__poll(rb, 1000 /* timeout, s */);
+			/* Ctrl-C will cause -EINTR */
+			if (err == -EINTR) {
+				err = 0;
+				break;
+			}
+			if (err < 0) {
+        	    printf("Error polling perf buffer: %d\n", err);
+				break;
+			}
+			/*打印直方图*/
+			histogram();
+		}
+		else {
+			printf("正在开发中, -c打印cs_delay, -s打印libbpf_sar\n");
+			break;
+		}
+	}
+	
+/* 卸载BPF程序 */
+cleanup:
+    /* Clean up */
+	if(env.cs_delay) ring_buffer__free(rb);//释放环形缓冲区
+	cpu_watcher_bpf__destroy(skel);
+	
+	return err < 0 ? -err : 0;
+}
