2be1dfa439fc62f6158ffab1fa3413fea11fcca9
xukanshan <1085714254@qq.com>
2023-12-29 10:21:29 +0800

    将MemSubsystem/pr procstat修改成单独可用状态，增加过滤用户态程序产生的数据功能，配置流水线

--- eBPF_Supermarket/Memory_Subsystem/applications/procstat/procstat.c
+++ eBPF_Supermarket/Memory_Subsystem/applications/procstat/procstat.c
@@ -9,12 +9,14 @@
 #include "procstat.h"
 #include "procstat.skel.h"
 #include <sys/select.h>
+#include <unistd.h>
 
- static struct env {
-        long choose_pid;
-        long time_s;
-    	long rss;
- } env; 
+static struct env
+{
+	long choose_pid;
+	long time_s;
+	long rss;
+} env;
 /*
 const char *argp_program_version = "bootstrap 0.0";
 const char *argp_program_bug_address = "<bpf@vger.kernel.org>";
@@ -28,37 +30,40 @@ const char argp_program_doc[] =
 "USAGE: ./bootstrap [-d <min-duration-ms>] [-v]\n";
 */
 static const struct argp_option opts[] = {
-        { "choose_pid", 'p', "PID", 0, "选择进程号打印。" },
-        { "time_s", 't', "MS", 0, "延时打印。单位：毫秒" },
-	{ "Rss", 'r', NULL, 0, "进程页面。"},
+	{"choose_pid", 'p', "PID", 0, "选择进程号打印。"},
+	{"time_s", 't', "MS", 0, "延时打印。单位：毫秒"},
+	{"Rss", 'r', NULL, 0, "进程页面。"},
 };
+
+pid_t own_pid;
 
 static error_t parse_arg(int key, char *arg, struct argp_state *state)
 {
-        switch (key) {
-        case 'p':
-                env.choose_pid = strtol(arg, NULL, 10);
-                break;
-        case 't':
+	switch (key)
+	{
+	case 'p':
+		env.choose_pid = strtol(arg, NULL, 10);
+		break;
+	case 't':
 		env.time_s = strtol(arg, NULL, 10);
-                break;
+		break;
 	case 'r':
 		env.rss = true;
-                break;
-        case ARGP_KEY_ARG:
-                argp_usage(state);
-                break;
-        default:
-                return ARGP_ERR_UNKNOWN;
-        }
-        return 0;
+		break;
+	case ARGP_KEY_ARG:
+		argp_usage(state);
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
 }
 
- static const struct argp argp = {
-        .options = opts,
-        .parser = parse_arg,
-//        .doc = argp_program_doc,
- };
+static const struct argp argp = {
+	.options = opts,
+	.parser = parse_arg,
+	//        .doc = argp_program_doc,
+};
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
@@ -71,14 +76,14 @@ static void sig_handler(int sig)
 {
 	exiting = true;
 }
- 
+
 static void msleep(long secs)
 {
 	struct timeval tval;
-	
-	tval.tv_sec=secs/1000;
-	tval.tv_usec=(secs*1000)%1000000;
-	select(0,NULL,NULL,NULL,&tval);
+
+	tval.tv_sec = secs / 1000;
+	tval.tv_usec = (secs * 1000) % 1000000;
+	select(0, NULL, NULL, NULL, &tval);
 }
 
 static int handle_event(void *ctx, void *data, size_t data_sz)
@@ -92,27 +97,32 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	tm = localtime(&t);
 	strftime(ts, sizeof(ts), "%H:%M:%S", tm);
 
-	if(env.choose_pid) {
-		if(e->pid == env.choose_pid) {
-			if(env.rss == true)
+	if (env.choose_pid)
+	{
+		if (e->pid == env.choose_pid)
+		{
+			if (env.rss == true)
 				printf("%-8s %-8d %-8ld %-8ld %-8ld %-8lld %-8lld\n", ts, e->pid, e->vsize, e->Vdata, e->Vstk, e->VPTE, e->vswap);
 			else
 				printf("%-8s %-8d %-8ld %-8lld %-8lld %-8lld\n", ts, e->pid, e->size, e->rssanon, e->rssfile, e->rssshmem);
 		}
 	}
-	else {
-		if(env.rss == true)
-				printf("%-8s %-8d %-8ld %-8ld %-8ld %-8lld %-8lld\n", ts, e->pid, e->vsize, e->Vdata, e->Vstk, e->VPTE, e->vswap);
-			else
-				printf("%-8s %-8d %-8ld %-8lld %-8lld %-8lld\n", ts, e->pid, e->size, e->rssanon, e->rssfile, e->rssshmem);
+	else
+	{
+		if (env.rss == true)
+			printf("%-8s %-8d %-8ld %-8ld %-8ld %-8lld %-8lld\n", ts, e->pid, e->vsize, e->Vdata, e->Vstk, e->VPTE, e->vswap);
+		else
+			printf("%-8s %-8d %-8ld %-8lld %-8lld %-8lld\n", ts, e->pid, e->size, e->rssanon, e->rssfile, e->rssshmem);
 	}
-
-	if(env.time_s != NULL) {
-		msleep(env.time_s);
-	}
-	else {
-		msleep(1000);
-	}
+	/* 睡眠会导致程序无法终止，所以需要注释掉这个代码块   */
+	// if (env.time_s != NULL)
+	// {
+	// 	msleep(env.time_s);
+	// }
+	// else
+	// {
+	// 	msleep(1000);
+	// }
 	return 0;
 }
 
@@ -122,9 +132,11 @@ int main(int argc, char **argv)
 	struct procstat_bpf *skel;
 	int err;
 
-        err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
-        if (err)
-                return err;
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
+
+	own_pid = getpid();
 
 	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
 	/* Set up libbpf errors and debug info callback */
@@ -136,49 +148,60 @@ int main(int argc, char **argv)
 
 	/* Load and verify BPF application */
 	skel = procstat_bpf__open();
-	if (!skel) {
+	if (!skel)
+	{
 		fprintf(stderr, "Failed to open and load BPF skeleton\n");
 		return 1;
 	}
 
+	skel->bss->user_pid = own_pid;
+	
 	/* Load & verify BPF programs */
 	err = procstat_bpf__load(skel);
-	if (err) {
+	if (err)
+	{
 		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
 		goto cleanup;
 	}
 
 	/* Attach tracepoints */
 	err = procstat_bpf__attach(skel);
-	if (err) {
+	if (err)
+	{
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
 	}
 
 	/* Set up ring buffer polling */
 	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
-	if (!rb) {
+	if (!rb)
+	{
 		err = -1;
 		fprintf(stderr, "Failed to create ring buffer\n");
 		goto cleanup;
 	}
 
 	/* Process events */
-	if(env.rss == true) {
+	if (env.rss == true)
+	{
 		printf("%-8s %-8s %-8s %-8s %-8s %-8s %-8s\n", "TIME", "PID", "VMSIZE", "VMDATA", "VMSTK", "VMPTE", "VMSWAP");
 	}
-	else{			
+	else
+	{
 		printf("%-8s %-8s %-8s %-8s %-8s %-8s\n", "TIME", "PID", "SIZE", "RSSANON", "RSSFILE", "RSSSHMEM");
 	}
 
-	while (!exiting) {
+	while (!exiting)
+	{
 		err = ring_buffer__poll(rb, 100 /* timeout, ms */);
 		/* Ctrl-C will cause -EINTR */
-		if (err == -EINTR) {
+		if (err == -EINTR)
+		{
 			err = 0;
 			break;
 		}
-		if (err < 0) {
+		if (err < 0)
+		{
 			printf("Error polling perf buffer: %d\n", err);
 			break;
 		}
