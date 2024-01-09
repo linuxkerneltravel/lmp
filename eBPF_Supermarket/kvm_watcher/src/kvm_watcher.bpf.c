fe2f275c9310a180996f00c6aabcfe8c3f73229c
nanshuaibo <nanshuaibo811@163.com>
2024-01-09 14:42:49 +0800

    kvm pic中断延时等信息提取，优化代码

--- eBPF_Supermarket/kvm_watcher/src/kvm_watcher.bpf.c
+++ eBPF_Supermarket/kvm_watcher/src/kvm_watcher.bpf.c
@@ -23,25 +23,32 @@
 #include "../include/kvm_exits.h"
 #include "../include/kvm_vcpu.h"
 #include "../include/kvm_mmu.h"
+#include "../include/kvm_pic.h"
 #include "../include/kvm_watcher.h"
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 const volatile pid_t vm_pid = -1;
+static struct common_event *e;
 
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 256 * 1024);
 } rb SEC(".maps");
 
+SEC("fentry/kvm_vcpu_halt")
+int BPF_PROG(fentry_kvm_vcpu_halt, struct kvm_vcpu *vcpu) {
+    return trace_kvm_vcpu_halt(vcpu, vm_pid);
+}
+
 SEC("tp/kvm/kvm_vcpu_wakeup")
 int tp_vcpu_wakeup(struct vcpu_wakeup *ctx) {
-    return trace_kvm_vcpu_wakeup(ctx, &rb, vm_pid);
+    return trace_kvm_vcpu_wakeup(ctx, &rb, e, vm_pid);
 }
 
 SEC("tp/kvm/kvm_halt_poll_ns")
 int tp_kvm_halt_poll_ns(struct halt_poll_ns *ctx) {
-    return trace_kvm_halt_poll_ns(ctx, &rb, vm_pid);
+    return trace_kvm_halt_poll_ns(ctx, &rb, e, vm_pid);
 }
 
 SEC("tp/kvm/kvm_exit")
@@ -51,13 +58,13 @@ int tp_exit(struct exit *ctx) {
 
 SEC("tp/kvm/kvm_entry")
 int tp_entry(struct exit *ctx) {
-    return trace_kvm_entry(&rb);
+    return trace_kvm_entry(&rb, e);
 }
 
 SEC("kprobe/mark_page_dirty_in_slot")
 int BPF_KPROBE(kp_mark_page_dirty_in_slot, struct kvm *kvm,
                const struct kvm_memory_slot *memslot, gfn_t gfn) {
-    return trace_mark_page_dirty_in_slot(kvm, memslot, gfn, &rb, vm_pid);
+    return trace_mark_page_dirty_in_slot(kvm, memslot, gfn, &rb, e, vm_pid);
 }
 
 SEC("tp/kvm/kvm_page_fault")
@@ -68,7 +75,7 @@ int tp_page_fault(struct trace_event_raw_kvm_page_fault *ctx) {
 SEC("fexit/direct_page_fault")
 int BPF_PROG(fexit_direct_page_fault, struct kvm_vcpu *vcpu,
              struct kvm_page_fault *fault) {
-    return trace_direct_page_fault(vcpu, fault, &rb);
+    return trace_direct_page_fault(vcpu, fault, &rb, e);
 }
 
 SEC("fentry/kvm_mmu_page_fault")
@@ -80,5 +87,18 @@ int BPF_PROG(fentry_kvm_mmu_page_fault, struct kvm_vcpu *vcpu, gpa_t cr2_or_gpa,
 SEC("fexit/handle_mmio_page_fault")
 int BPF_PROG(fexit_handle_mmio_page_fault, struct kvm_vcpu *vcpu, u64 addr,
              bool direct) {
-    return trace_handle_mmio_page_fault(vcpu, addr, direct, &rb);
+    return trace_handle_mmio_page_fault(vcpu, addr, direct, &rb, e);
+}
+
+SEC("fentry/kvm_pic_set_irq")
+int BPF_PROG(fentry_kvm_pic_set_irq, struct kvm_pic *s, int irq,
+             int irq_source_id, int level) {
+    return trace_in_kvm_pic_set_irq(s, irq, irq_source_id, level, vm_pid);
+}
+
+SEC("fexit/kvm_pic_set_irq")
+int BPF_PROG(fexit_kvm_pic_set_irq, struct kvm_pic *s, int irq,
+             int irq_source_id, int level, int retval) {
+    return trace_out_kvm_pic_set_irq(s, irq, irq_source_id, level, retval, &rb,
+                                     e);
 }
