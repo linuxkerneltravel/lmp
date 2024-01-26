28bfef33f285df59aac903d5114036ac26e81a41
shangfan <45649554+sf1999817@users.noreply.github.com>
2024-01-26 05:47:49 +0000

    libbpf_learning
    Signed-off-by: shangfan <45649554+sf1999817@users.noreply.github.com>

---
+++ eBPF_Supermarket/Filesystem_Subsystem/fast_fuse/fusedemo/open.h
@@ -0,0 +1,16 @@
+#ifndef __OPEN_H
+#define __OPEN_H
+
+#ifndef TASK_COMM_LEN
+#define TASK_COMM_LEN 16
+#endif
+
+struct fs_t {
+	int pid;
+	int uid;
+    	int fd;
+    	long unsigned int ts;
+    	char comm[TASK_COMM_LEN];
+};
+
+#endif /* __OPEN_H */
