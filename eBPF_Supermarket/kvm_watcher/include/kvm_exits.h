dbb0fc656ea959c7a26e701973c920ed22ce4b8a
YYS <yys2020haha@163.com>
2024-03-01 12:46:49 +0800

    VMexit

--- eBPF_Supermarket/kvm_watcher/include/kvm_exits.h
+++ eBPF_Supermarket/kvm_watcher/include/kvm_exits.h
@@ -24,21 +24,25 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
-// 定义哈希结构，存储时间信息
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 8192);
-    __type(key, pid_t);
-    __type(value, struct reason_info);
-} times SEC(".maps");
+
+#define  EXIT_REASON_HLT 12
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 8192);
-    __type(key, u32);
-    __type(value, u32);
-} counts SEC(".maps");
-// 记录退出的信息
+    __type(key, struct exit_key); //exit_key:reason pid pad[2]
+    __type(value, struct exit_value); //exit_value : max_time total_time min_time count pad
+} exit_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 8192);
+    __type(key, pid_t);
+    __type(value, struct reason_info); //reason_info:time、reason、count
+} times SEC(".maps");
+
+
+
 struct exit {
     u64 pad;
     unsigned int exit_reason;
@@ -51,38 +55,29 @@ struct exit {
     unsigned int vcpu_id;
 };
 
-int total = 0;
-// 记录vm_exit的原因以及时间
 static int trace_kvm_exit(struct exit *ctx, pid_t vm_pid) {
     CHECK_PID(vm_pid);
+    u32 reason;
+    reason = (u32)ctx->exit_reason;
+    //如果是节能停止退出，就不采集数据
+    if(reason==EXIT_REASON_HLT){
+        return 0;
+    }
     u64 id, ts;
     id = bpf_get_current_pid_tgid();
     pid_t tid = (u32)id;
     ts = bpf_ktime_get_ns();
-    u32 reason;
-    reason = (u32)ctx->exit_reason;
     struct reason_info reas = {};
     reas.reason = reason;
     reas.time = ts;
-    u32 *count;
-    count = bpf_map_lookup_elem(&counts, &reason);
-    if (count) {
-        (*count)++;
-        reas.count = *count;
-        bpf_map_update_elem(&counts, &reason, count, BPF_ANY);
-    } else {
-        u32 new_count = 1;
-        reas.count = new_count;
-        bpf_map_update_elem(&counts, &reason, &new_count, BPF_ANY);
-    }
     bpf_map_update_elem(&times, &tid, &reas, BPF_ANY);
     return 0;
 }
-// 通过kvm_exit所记录的信息，来计算出整个处理的时间
-static int trace_kvm_entry(void *rb, struct common_event *e) {
+
+static int trace_kvm_entry() {
     struct reason_info *reas;
     pid_t pid, tid;
-    u64 id, ts, *start_ts, duration_ns = 0;
+    u64 id, ts, *start_ts, duration_ns;
     id = bpf_get_current_pid_tgid();
     pid = id >> 32;
     tid = (u32)id;
@@ -90,22 +85,32 @@ static int trace_kvm_entry(void *rb, struct common_event *e) {
     if (!reas) {
         return 0;
     }
-    u32 reason;
-    int count = 0;
     duration_ns = bpf_ktime_get_ns() - reas->time;
     bpf_map_delete_elem(&times, &tid);
-    reason = reas->reason;
-    count = reas->count;
-    RESERVE_RINGBUF_ENTRY(rb, e);
-    e->exit_data.reason_number = reason;
-    e->process.pid = pid;
-    e->process.tid = tid;
-    e->exit_data.duration_ns = duration_ns;
-    bpf_get_current_comm(&e->process.comm, sizeof(e->process.comm));
-    e->exit_data.total = ++total;
-    e->exit_data.count = count;
-    e->time = reas->time;
-    bpf_ringbuf_submit(e, 0);
+    struct exit_key exit_key;
+    __builtin_memset(&exit_key, 0, sizeof(struct exit_key));
+    exit_key.pid=pid;
+    exit_key.reason=reas->reason;
+    struct exit_value *exit_value;
+    exit_value = bpf_map_lookup_elem(&exit_map, &exit_key);
+    if (exit_value) {
+        exit_value->count ++;
+        exit_value->total_time +=duration_ns;
+        if(exit_value->max_time < duration_ns){
+            exit_value->max_time = duration_ns;
+        }
+        if(exit_value->min_time > duration_ns){
+            exit_value->min_time=duration_ns;
+        }
+    } else {
+        struct exit_value new_exit_value = {
+            .count=1,
+            .max_time=duration_ns,
+            .total_time=duration_ns,
+            .min_time=duration_ns
+        };
+        bpf_map_update_elem(&exit_map, &exit_key, &new_exit_value, BPF_ANY);
+    }
     return 0;
 }
 #endif /* __KVM_EXITS_H */
