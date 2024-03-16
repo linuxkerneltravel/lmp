c1c8c0851907ef3d19e3ebd49afb6834acd7de4c
LiuLingze <luiyanbing@foxmail.com>
2024-03-16 08:18:16 +0000

    update file header
    Signed-off-by: LiuLingze <luiyanbing@foxmail.com>

--- MagicEyes/src/backend/system_diagnosis/stack_analyzer/include/bpf/template.h
+++ MagicEyes/src/backend/system_diagnosis/stack_analyzer/include/bpf/template.h
@@ -1,3 +1,21 @@
+// Copyright 2024 The LMP Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/linuxkerneltravel/lmp/blob/develop/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// author: luiyanbing@foxmail.com
+//
+// ebpf程序的包装类的模板，声明接口和一些自定义方法，以及辅助结构
+
 #ifndef _TEMPLATE_H__
 #define _TEMPLATE_H__
 
