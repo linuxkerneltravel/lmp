7639b856d83aa2134a072dc9526179b25ed87ff2
syxl-time <953879556@qq.com>
2024-03-18 17:07:15 +0800

    mem_watcher:添加内存泄露工具

--- eBPF_Supermarket/Memory_Subsystem/applications/mem_watcher/mem_watcher.c
+++ eBPF_Supermarket/Memory_Subsystem/applications/mem_watcher/mem_watcher.c
@@ -16,7 +16,9 @@
 //
 // mem_watcher libbpf user mode code
 
+#include <assert.h>
 #include <argp.h>
+#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <time.h>
@@ -28,7 +30,17 @@
 #include "pr.skel.h"
 #include "procstat.skel.h"
 #include "sysstat.skel.h"
+#include "memleak.skel.h"
 #include "mem_watcher.h"
+
+#include "blazesym.h"
+
+static const int perf_max_stack_depth = 127;    //stack id 对应的堆栈的深度
+static const int stack_map_max_entries = 10240; //最大允许存储多少个stack_id
+static __u64 *g_stacks = NULL;
+static size_t g_stacks_size = 0;
+
+static struct blaze_symbolizer *symbolizer;
 
 static struct env {
 	int time;
@@ -36,6 +48,7 @@ static struct env {
 	bool pr;
 	bool procstat;
 	bool sysstat;
+	bool memleak;
 
 	bool part2;
 
@@ -47,6 +60,7 @@ static struct env {
 	.pr = false,
 	.procstat = false,
 	.sysstat = false,
+	.memleak = false,
 	.rss = false,
 	.part2 = false,
 };
@@ -69,7 +83,9 @@ static const struct argp_option opts[] = {
 	{0, 0, 0, 0, "sysstat additional function:"},
 	{"part2", 'n', NULL, 0, "系统内存状态报告2", 7},
 
-	{"time", 't', "TIME-SEC", 0, "Max Running Time(0 for infinite)", 8},
+	{"memleak", 'l', "PID", 0, "print memleak (内存泄漏检测)", 8},
+
+	{"time", 't', "TIME-SEC", 0, "Max Running Time(0 for infinite)", 9},
 	{NULL, 'h', NULL, OPTION_HIDDEN, "show the full help"},
 	{0},
 };
@@ -105,6 +121,9 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state) {
 	case 'R':
 		env.rss = true;
 		break;
+	case 'l':
+		env.memleak = true;
+		break;
 	default:
 		return ARGP_ERR_UNKNOWN;
 	}
@@ -117,6 +136,122 @@ static const struct argp argp = {
 	.doc = argp_program_doc,
 };
 
+static void print_frame(const char *name, uintptr_t input_addr, uintptr_t addr, uint64_t offset, const blaze_symbolize_code_info* code_info)
+{
+    // If we have an input address  we have a new symbol.
+    if (input_addr != 0) {
+      printf("%016lx: %s @ 0x%lx+0x%lx", input_addr, name, addr, offset);
+			if (code_info != NULL && code_info->dir != NULL && code_info->file != NULL) {
+				printf(" %s/%s:%u\n", code_info->dir, code_info->file, code_info->line);
+      } else if (code_info != NULL && code_info->file != NULL) {
+				printf(" %s:%u\n", code_info->file, code_info->line);
+      } else {
+				printf("\n");
+      }
+    } else {
+      printf("%16s  %s", "", name);
+			if (code_info != NULL && code_info->dir != NULL && code_info->file != NULL) {
+				printf("@ %s/%s:%u [inlined]\n", code_info->dir, code_info->file, code_info->line);
+      } else if (code_info != NULL && code_info->file != NULL) {
+				printf("@ %s:%u [inlined]\n", code_info->file, code_info->line);
+      } else {
+				printf("[inlined]\n");
+      }
+    }
+}
+
+static void show_stack_trace(__u64 *stack, int stack_sz, pid_t pid) {
+    const struct blaze_symbolize_inlined_fn *inlined;
+    const struct blaze_result *result;
+    const struct blaze_sym *sym;
+    int i, j;
+
+    assert(sizeof(uintptr_t) == sizeof(uint64_t));
+
+    if (pid) {
+        struct blaze_symbolize_src_process src = {
+            .pid = pid,
+        };
+        result = blaze_symbolize_process(symbolizer, &src, (const uintptr_t *)stack, stack_sz);
+    }
+    else {
+        struct blaze_symbolize_src_kernel src = {};
+        result = blaze_symbolize_kernel(symbolizer, &src, (const uintptr_t *)stack, stack_sz);
+    }
+
+
+    for (i = 0; i < stack_sz; i++) {
+        if (!result || result->cnt <= i || result->syms[i].name == NULL) {
+            printf(" %2d [<%016llx>]\n", i, stack[i]);
+            continue;
+        }
+
+        sym = &result->syms[i];
+        print_frame(sym->name, stack[i], sym->addr, sym->offset, &sym->code_info);
+
+        for (j = 0; j < sym->inlined_cnt; j++) {
+            inlined = &sym->inlined[j];
+            print_frame(sym->name, 0, 0, 0, &inlined->code_info);
+        }
+    }
+
+    blaze_result_free(result);
+}
+
+int print_outstanding_combined_allocs(struct memleak_bpf *skel, pid_t pid) {
+    const size_t combined_allocs_key_size = bpf_map__key_size(skel->maps.combined_allocs);
+    const size_t stack_traces_key_size = bpf_map__key_size(skel->maps.stack_traces);
+
+    for (__u64 prev_key = 0, curr_key = 0; ; prev_key = curr_key) {
+
+        if (bpf_map__get_next_key(skel->maps.combined_allocs,
+            &prev_key, &curr_key, combined_allocs_key_size)) {
+            if (errno == ENOENT) {
+                break; //no more keys, done!
+            }
+            perror("map get next key failed!");
+
+            return -errno;
+        }
+
+        // stack_id = curr_key
+        union combined_alloc_info cinfo;
+        memset(&cinfo, 0, sizeof(cinfo));
+
+        if (bpf_map__lookup_elem(skel->maps.combined_allocs,
+            &curr_key, combined_allocs_key_size, &cinfo, sizeof(cinfo), 0)) {
+            if (errno == ENOENT) {
+                continue;
+            }
+
+            perror("map lookup failed!");
+            return -errno;
+        }
+
+        if (bpf_map__lookup_elem(skel->maps.stack_traces,
+            &curr_key, stack_traces_key_size, g_stacks, g_stacks_size, 0)) {
+            perror("failed to lookup stack traces!");
+            return -errno;
+        }
+
+        printf("stack_id=0x%llx with outstanding allocations: total_size=%llu nr_allocs=%llu\n",
+            curr_key, (__u64)cinfo.total_size, (__u64)cinfo.number_of_allocs);
+
+        int stack_sz = 0;
+        for (int i = 0; i < perf_max_stack_depth; i++) {
+            if (g_stacks[i] == 0) {
+                break;
+            }
+            stack_sz++;
+            //printf("[%3d] 0x%llx\n", i, g_stacks[i]);
+        }
+
+        show_stack_trace(g_stacks, stack_sz, pid);
+    }
+
+    return 0;
+}
+
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
 	return vfprintf(stderr, format, args);
 }
@@ -125,6 +260,7 @@ static volatile bool exiting = false;
 
 static void sig_handler(int sig) {
 	exiting = true;
+	exit(EXIT_SUCCESS); 
 }
 
 /*
@@ -242,13 +378,19 @@ int main(int argc, char **argv) {
 	struct pr_bpf *skel_pr;
 	struct procstat_bpf *skel_procstat;
 	struct sysstat_bpf *skel_sysstat;
+	struct memleak_bpf *skel;
 
-	int err;
+	int err, i;
+	LIBBPF_OPTS(bpf_uprobe_opts, uprobe_opts);
+	int attach_pid;
+	char binary_path[128] = { 0 };
+
 	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
 	if (err)
 		return err;
 	own_pid = getpid();
 	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+
 	/* Set up libbpf errors and debug info callback */
 	libbpf_set_print(libbpf_print_fn);
 
@@ -410,8 +552,105 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	else if (env.memleak) {
+		if (argc != 3) {
+			printf("usage:%s attach_pid\n", argv[0]);
+			return -1;
+		}
+
+		attach_pid = atoi(argv[2]);
+
+		strcpy(binary_path, "/lib/x86_64-linux-gnu/libc.so.6");
+
+		/* Set up libbpf errors and debug info callback */
+		libbpf_set_print(libbpf_print_fn);
+
+		/* Load and verify BPF application */
+		skel = memleak_bpf__open();
+		if (!skel) {
+			fprintf(stderr, "Failed to open BPF skeleton\n");
+			return 1;
+		}
+
+		bpf_map__set_value_size(skel->maps.stack_traces, perf_max_stack_depth * sizeof(__u64));
+		bpf_map__set_max_entries(skel->maps.stack_traces, stack_map_max_entries);
+
+		/* Load & verify BPF programs */
+		err = memleak_bpf__load(skel);
+		if (err) {
+			fprintf(stderr, "Failed to load BPF skeleton\n");
+			goto memleak_cleanup;
+		}
+
+		/* Attach tracepoint handler */
+		uprobe_opts.func_name = "malloc";
+		uprobe_opts.retprobe = false;
+		/* uprobe/uretprobe expects relative offset of the function to attach
+		 * to. libbpf will automatically find the offset for us if we provide the
+		 * function name. If the function name is not specified, libbpf will try
+		 * to use the function offset instead.
+		 */
+		skel->links.malloc_enter = bpf_program__attach_uprobe_opts(skel->progs.malloc_enter,
+			attach_pid /* self pid */, binary_path,
+			0 /* offset for function */,
+			&uprobe_opts /* opts */);
+		if (!skel->links.malloc_enter) {
+			err = -errno;
+			fprintf(stderr, "Failed to attach uprobe: %d\n", err);
+			goto memleak_cleanup;
+		}
+
+		uprobe_opts.func_name = "malloc";
+		uprobe_opts.retprobe = true;
+		skel->links.malloc_exit = bpf_program__attach_uprobe_opts(skel->progs.malloc_exit,
+			attach_pid, binary_path,
+			0, &uprobe_opts);
+		if (!skel->links.malloc_exit) {
+			err = -errno;
+			fprintf(stderr, "Failed to attach uprobe: %d\n", err);
+			goto memleak_cleanup;
+		}
+
+		uprobe_opts.func_name = "free";
+		uprobe_opts.retprobe = false;
+		skel->links.free_enter = bpf_program__attach_uprobe_opts(skel->progs.free_enter,
+			attach_pid, binary_path,
+			0, &uprobe_opts);
+		if (!skel->links.free_enter) {
+			err = -errno;
+			fprintf(stderr, "Failed to attach uprobe: %d\n", err);
+			goto memleak_cleanup;
+		}
+
+		/* Let libbpf perform auto-attach for uprobe_sub/uretprobe_sub
+		 * NOTICE: we provide path and symbol info in SEC for BPF programs
+		 */
+		err = memleak_bpf__attach(skel);
+		if (err) {
+			fprintf(stderr, "Failed to auto-attach BPF skeleton: %d\n", err);
+			goto memleak_cleanup;
+		}
+
+		g_stacks_size = perf_max_stack_depth * sizeof(*g_stacks);
+		g_stacks = (__u64 *)malloc(g_stacks_size);
+		memset(g_stacks, 0, g_stacks_size);
+
+		symbolizer = blaze_symbolizer_new();
+		if (!symbolizer) {
+			fprintf(stderr, "Fail to create a symbolizer\n");
+			err = -1;
+			goto memleak_cleanup;
+		}
+
+		for (i = 0;; i++) {
+			/* trigger our BPF programs */
+			print_outstanding_combined_allocs(skel, attach_pid);
+			sleep(1);
+		}
+	}
+
 	while (!exiting) {
-		if (env.paf) {
+		if (env.paf || env.pr || env.procstat || env.sysstat) {
 			err = ring_buffer__poll(rb, 1000 /* timeout, ms */);
 			/* Ctrl-C will cause -EINTR */
 			if (err == -EINTR) {
@@ -423,32 +662,7 @@ int main(int argc, char **argv) {
 				break;
 			}
 		}
-		else if (env.pr) {
-			err = ring_buffer__poll(rb, 100 /* timeout, ms */);
-			/* Ctrl-C will cause -EINTR */
-			if (err == -EINTR) {
-				err = 0;
-				break;
-			}
-			if (err < 0) {
-				printf("Error polling perf buffer: %d\n", err);
-				break;
-			}
-		}
-		else if (env.procstat) {
-			err = ring_buffer__poll(rb, 100 /* timeout, ms */);
-			/* Ctrl-C will cause -EINTR */
-			if (err == -EINTR) {
-				err = 0;
-				break;
-			}
-			if (err < 0) {
-				printf("Error polling perf buffer: %d\n", err);
-				break;
-			}
-		}
-		else if (env.sysstat) {
-			err = ring_buffer__poll(rb, 100 /* timeout, ms */);
+		else if (env.memleak) {
 			/* Ctrl-C will cause -EINTR */
 			if (err == -EINTR) {
 				err = 0;
@@ -483,5 +697,11 @@ procstat_cleanup:
 sysstat_cleanup:
 	ring_buffer__free(rb);
 	sysstat_bpf__destroy(skel_sysstat);
+	return err < 0 ? -err : 0;
+
+memleak_cleanup:
+	memleak_bpf__destroy(skel);
+	blaze_symbolizer_free(symbolizer);
+	free(g_stacks);
 	return err < 0 ? -err : 0;
 }
