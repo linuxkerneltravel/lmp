68aedd92bb1711a4d9491f8660ed96f3a441fdec
albertxu216 <1418106416@qq.com>
2024-03-25 12:14:39 +0800

    Add mq_delay

--- eBPF_Supermarket/CPU_Subsystem/cpu_watcher/mq_delay.bpf.c
+++ eBPF_Supermarket/CPU_Subsystem/cpu_watcher/mq_delay.bpf.c
@@ -12,78 +12,87 @@ BPF_PERCPU_HASH(Time,mqd_t,struct events,1024);//记录事件
 
 BPF_PERCPU_HASH(key,int,mqd_t,1024);//通过pid帮助exit_receive找到mqdes；
 
+BPF_PERCPU_HASH(get_msg1,pid_t,struct events,1024);//记录pid->u_msg_ptr的关系；do_mq_timedsend入参
+BPF_PERCPU_HASH(get_msg,struct msg_msg,u64,1024);//记录msg->time的关系；
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
 	__uint(max_entries, 256 * 1024);
 } rb SEC(".maps");
 
 
-
 SEC("kprobe/do_mq_timedsend")
-int BPF_KPROBE(mq_timedsend,mqd_t mqdes, const char __user *u_msg_ptr,
+int BPF_KPROBE(mq_timedsend,mqd_t mqdes, const char *u_msg_ptr,
 		size_t msg_len, unsigned int msg_prio,
 		struct timespec64 *ts)
 {
 	u64 send_enter_time = bpf_ktime_get_ns();//开始发送信息时间；
 	int pid = bpf_get_current_pid_tgid();//发送端pid
-	//bpf_map_update_elem(&key, &pid, &mqdes, BPF_ANY);//记录发送端pid与mqdes键值
-	//bpf_printk("send_pid: %-8llu ,mqdes: %-8llu, msg_len: %-8d,  msg_prio: %-8lu\n",pid,mqdes,msg_len,msg_prio);
 
-	/*记录数值*/
-	struct events val={};
-	val.send_pid =  pid;
-	val.mqdes = mqdes;
-	val.send_enter_time = send_enter_time;
-	bpf_map_update_elem(&Time, &mqdes, &val, BPF_ANY);	
+	/*赋值*/
+	struct events mq_info ={};
+	mq_info.send_pid= pid;
+	mq_info.send_enter_time = send_enter_time;
+		mq_info.mqdes= mqdes;
+		mq_info.msg_len = msg_len;
+		mq_info.msg_prio = msg_prio;
+		mq_info.u_msg_ptr = u_msg_ptr;
 
-
+	bpf_map_update_elem(&get_msg1, &pid, &mq_info, BPF_ANY);//pid->u_msg_ptr
+	// bpf_map_update_elem(&get_msg2, &pid, &send_enter_time, BPF_ANY);//pid->time	
 	return 0;	
 } 	
-
-SEC("kprobe/do_mq_timedreceive")
-int BPF_KPROBE(mq_timedreceive_entry,mqd_t mqdes, const char __user *u_msg_ptr,
-		size_t msg_len, unsigned int msg_prio,
-		struct timespec64 *ts)
-{
-	u64 rcv_enter_time = bpf_ktime_get_ns();
-	int pid = bpf_get_current_pid_tgid();
-	bpf_map_update_elem(&key, &pid, &mqdes, BPF_ANY);//记录接收端pid与mqdes键值
-
-
-	return 0;
-}
-
-SEC("kretprobe/do_mq_timedreceive")
-int BPF_KRETPROBE(mq_timedreceive_exit)
-{
-	u64 rcv_finish_time = bpf_ktime_get_ns();
-	int pid = bpf_get_current_pid_tgid();
-	u64 delay;
-	/*通过接收进程的pid，获取消息队列的消息号mqdes*/
-	mqd_t *mqdes = bpf_map_lookup_elem(&key, &pid);
-	if(!mqdes) return 0;
-
-	/*通过mqdes，获取发送消息时记录的信息*/
-	struct events *val = bpf_map_lookup_elem(&Time, mqdes);
-	if(!val){
+SEC("kprobe/load_msg")
+int BPF_KPROBE(load_msg_enter,const void *src, size_t len){
+	int pid = bpf_get_current_pid_tgid();//发送端pid
+	/*记录load入参src*/
+	struct events *mq_info = bpf_map_lookup_elem(&get_msg1, &pid);
+	if(!mq_info){
 		return 0;
-	}else delay = (rcv_finish_time - val->send_enter_time)/1000000000;
+	}else{
+		mq_info->src = src;
+	} 	
+	bpf_map_update_elem(&get_msg1, &pid, mq_info, BPF_ANY);//pid->src
+
+	return 0;		
+}
+SEC("kretprobe/load_msg")
+int BPF_KRETPROBE(load_msg_exit,void *ret){
+	int pid = bpf_get_current_pid_tgid();//发送端pid
+	/*构建消息块结构体，作为key*/
+	struct msg_msg *tmp =(struct msg_msg *)ret;
+	struct events *mq_info = bpf_map_lookup_elem(&get_msg1, &pid);
+	if(!mq_info){
+		bpf_printk("erro2");
+		return 0;
+	}
+	/*make key*/
+	mq_info->Key_msg_ptr = (struct msg_msg){0};
+	mq_info->Key_msg_ptr.m_list =  BPF_CORE_READ(tmp,m_list);
+	mq_info->Key_msg_ptr.m_type =  BPF_CORE_READ(tmp,m_type);
+	mq_info->Key_msg_ptr.m_ts =  BPF_CORE_READ(tmp,m_ts);
+	mq_info->Key_msg_ptr.next =  BPF_CORE_READ(tmp,next);
+	mq_info->Key_msg_ptr.security =  BPF_CORE_READ(tmp,security);
+	mq_info->Key_msg_ptr.m_type = mq_info->msg_prio;
+	mq_info->Key_msg_ptr.m_ts = mq_info->msg_len;
 
 	struct events *e;
 	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
 	if (!e)	return 0;
 
-	e->send_pid = val->send_pid;
-	e->rcv_pid = pid;//获取到当前进程的pid
-	e->mqdes = val->mqdes;
-		e->send_enter_time = val->send_enter_time;
-		e->rcv_enter_time = val->rcv_enter_time;
-		e->rcv_exit_time = rcv_finish_time;
-		e->delay = delay;
+	e->send_pid = mq_info->send_pid;
+	e->send_enter_time = mq_info->send_enter_time;
+	e->mqdes = mq_info->mqdes;
+	e->msg_len = mq_info->msg_len;
+	e->msg_prio = mq_info->msg_prio;
+	e->u_msg_ptr = mq_info->u_msg_ptr;
+	e->src = mq_info->src;
 
-	bpf_map_delete_elem(&Time, &mqdes);//暂时性删除
+	e->m_type = BPF_CORE_READ(tmp,m_type);
+	e->m_ts = BPF_CORE_READ(tmp,m_ts);
+	e->Key_msg_ptr = mq_info->Key_msg_ptr;
+	bpf_map_delete_elem(&get_msg1, &pid);//暂时性删除
+
 	bpf_ringbuf_submit(e, 0);
 
-
-	return 0;
+	return 0;		
 }
