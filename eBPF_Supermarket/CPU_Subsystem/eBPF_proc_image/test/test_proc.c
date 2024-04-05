bc4e4a7dcac94b8eafd494712860728f7824feaf
zhangzihengya <zhangziheng0525@163.com>
2024-04-05 15:45:50 +0800

    add resource test

--- eBPF_Supermarket/CPU_Subsystem/eBPF_proc_image/test/test_proc.c
+++ eBPF_Supermarket/CPU_Subsystem/eBPF_proc_image/test/test_proc.c
@@ -119,7 +119,7 @@ int main(int argc, char **argv){
    pid = getpid();
    printf("test_proc进程的PID:%d\n", pid);
    printf("输入任意数字继续程序的运行:");
-   scanf("%d",&stop);                   // 使用时将其取消注释
+//   scanf("%d",&stop);                   // 使用时将其取消注释
    printf("程序开始执行...\n");
    printf("\n");
 
