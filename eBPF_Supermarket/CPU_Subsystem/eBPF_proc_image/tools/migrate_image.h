9eb7a26dd1fab3b7bf0736262242438a115ca87c
徐晗博 <albert_xuu@163.com>
2024-06-21 18:39:35 +0800

    migrate_image工具采集更多数据

--- eBPF_Supermarket/CPU_Subsystem/eBPF_proc_image/tools/migrate_image.h
+++ eBPF_Supermarket/CPU_Subsystem/eBPF_proc_image/tools/migrate_image.h
@@ -6,12 +6,29 @@ typedef unsigned int u32;
 /*----------------------------------------------*/
 /*         migrate_event结构体                     */
 /*----------------------------------------------*/
-#define MAX_MIGRATE 16
+#define MAX_MIGRATE 1024
+#define PAGE_SHIFT 13
 // #define ARRY_OVERFLOW -1
+struct minfo_key{
+	pid_t pid;
+	int count;
+};
 struct per_migrate{//每次迁移,记录该次迁移信息;
 	u64 time;
 	u32 orig_cpu;
 	u32 dest_cpu;
+	u64 orig_cpu_load;
+	u64 dest_cpu_load;
+	u64 pload_avg;
+	u64 putil_avg;
+	int on_cpu;
+    u64 mem_usage;
+    u64 read_bytes;
+	u64 write_bytes;
+	// u64 syscr;
+	// u64 syscw;
+    u64 context_switches;
+    u64 runtime;
 };
 //每个进程的迁移信息;
 struct migrate_event{
@@ -19,5 +36,5 @@ struct migrate_event{
 	pid_t pid;
 	int prio;
 	int count,rear;//迁移频率
-	struct per_migrate migrate_info[MAX_MIGRATE];//该进程每次迁移信息;
+	//struct per_migrate *migrate_info;//该进程每次迁移信息;
 };
