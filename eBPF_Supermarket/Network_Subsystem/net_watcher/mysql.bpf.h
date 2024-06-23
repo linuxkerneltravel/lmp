3f06000aad5103c167d72b1038f475097121bc69
wynyibo <wyn18091378056@163.com>
2024-06-23 21:45:53 +0800

    update

--- eBPF_Supermarket/Network_Subsystem/net_watcher/mysql.bpf.h
+++ eBPF_Supermarket/Network_Subsystem/net_watcher/mysql.bpf.h
@@ -27,15 +27,17 @@ static __always_inline int __handle_mysql_start(struct pt_regs *ctx) {
     struct query_info info;
     u32 size = 0;
     char *sql;
+    
     if (command != COM_QUERY) {
         return 0;
     }
-    bpf_probe_read(&info.size, sizeof(info.size),
-                   &com_data->com_query.length);
+
+    bpf_probe_read(&info.size, sizeof(info.size), &com_data->com_query.length);
     bpf_probe_read_str(&sql, sizeof(sql), &com_data->com_query.query);
     bpf_probe_read_str(&info.msql, sizeof(info.msql), sql);
     // bpf_printk("sql1==%s size1==%lu", info.msql,info.size);
     info.start_time = bpf_ktime_get_ns() / 1000;
+
     bpf_map_update_elem(&queries, &tid, &info, BPF_ANY);
     return 0;
 }
@@ -44,13 +46,10 @@ static __always_inline int __handle_mysql_end(struct pt_regs *ctx) {
     char comm[16];
     pid_t pid = bpf_get_current_pid_tgid() >> 32;
     pid_t tid = bpf_get_current_pid_tgid();
-    struct query_info *info = bpf_map_lookup_elem(&queries,&tid);
-    if(!info)
-    {
+    struct query_info *info = bpf_map_lookup_elem(&queries, &tsid);
+    if (!info) {
         return 0;
     }
-    bpf_printk("sql2==%s size2==%lu", info->msql,info->size);
-    u64 *start_time_ptr, duration;
 
     struct mysql_query *message =
         bpf_ringbuf_reserve(&mysql_rb, sizeof(*message), 0);
@@ -65,13 +64,15 @@ static __always_inline int __handle_mysql_end(struct pt_regs *ctx) {
 
     message->count = count;
     bpf_map_update_elem(&sql_count, &tid, &count, BPF_ANY);
-    message->duratime = bpf_ktime_get_ns()/1000-info->start_time;
+    message->duratime = bpf_ktime_get_ns() / 1000 - info->start_time;
     message->pid = pid;
     message->tid = tid;
     bpf_get_current_comm(&message->comm, sizeof(comm));
     message->size = info->size;
     bpf_probe_read_str(&message->msql, sizeof(message->msql), info->msql);
-  //  bpf_printk("C==%d D==%lu S==%lu SQL==%s",count, message->duratime,message->size,message->msql);
+    //  bpf_printk("C==%d D==%lu S==%lu SQL==%s",count,
+    //  message->duratime,message->size,message->msql);
+
     bpf_ringbuf_submit(message, 0);
     return 0;
 }
