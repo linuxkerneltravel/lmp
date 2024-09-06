7f9050b94d3e6584117ba938c11bcd451c445297
张铭轩 <11397959+zhangmingxuan777@user.noreply.gitee.com>
2024-09-06 19:26:48 +0800

    add the redis and stat the key

--- eBPF_Supermarket/Network_Subsystem/net_watcher/netwatcher.h
+++ eBPF_Supermarket/Network_Subsystem/net_watcher/netwatcher.h
@@ -192,6 +192,14 @@ struct redis_query {
     u64 begin_time;
     int argc;
 };
+struct redis_stat_query {
+    int pid;
+    char comm[20];
+    char key[20];
+    int key_count;
+    char value[64];
+    int value_type;
+};
 
 struct RTT {
     u32 saddr;
